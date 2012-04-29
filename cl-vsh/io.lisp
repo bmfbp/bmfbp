0a8b6b812d5daff289103ffcffa21e3605544b4e
Paul Tarvydas <paul.tarvydas@rogers.com>
2012-04-29 14:51:54 -0400

    added lic. removed progress, fixed write-p in io.lisp

--- cl-vsh/io.lisp
+++ cl-vsh/io.lisp
@@ -1,3 +1,33 @@
+;;; Copyright (c) 2012, Paul Tarvydas
+;;; All rights reserved.
+
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+
+;;;    Redistributions of source code must retain the above copyright
+;;;    notice, this list of conditions and the following disclaimer.
+
+;;;    Redistributions in binary form must reproduce the above
+;;;    copyright notice, this list of conditions and the following
+;;;    disclaimer in the documentation and/or other materials provided
+;;;    with the distribution.
+
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+;;; CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+;;; INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+;;; MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+;;; BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+;;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+;;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+;;; THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+;;; SUCH DAMAGE.
+
+
 ;; read facts from stdin, determine ownership of ports, output net (for semantic checking and coding)
 
 ;; the factbase is a hash table of hash tables
@@ -56,8 +86,9 @@
        (loop while (not (eq 'EOF fact))
              do (when (eq 'QUIT (first fact))
                   (funcall func facts)
-                  (write-facts facts outstrm)
-                  (format outstrm "(quit)~%")
+                  (when write-p
+		    (write-facts facts outstrm)
+		    (format outstrm "(quit)~%"))
                   #-lispworks (quit)
                   #+lispworks (return-from read-write-facts))
              do (add-fact facts fact legal-fact)  ;; facts can arrive in random order, cache them until ready
