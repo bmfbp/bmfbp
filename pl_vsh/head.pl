8e8d4555a3f6a0ce4d47c944127fb3edc1d49176
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-05-20 08:40:34 -0400

    Prolog version baseline

---
+++ pl_vsh/head.pl
@@ -0,0 +1,22 @@
+
+:- dynamic(eltype/2) .
+:- dynamic(kind/2) .
+:- dynamic(portName/2) .
+:- dynamic(geometry_x/2) .
+:- dynamic(geometry_y/2) .
+:- dynamic(geometry_w/2) .
+:- dynamic(geometry_h/2) .
+:- dynamic(node/1) .
+:- dynamic(component/1) .
+:- dynamic(edge/1) .
+:- dynamic(source/2) .
+:- dynamic(sink/2) .
+:- dynamic(bounding_box_left/2).
+:- dynamic(bounding_box_top/2).
+:- dynamic(bounding_box_right/2).
+:- dynamic(bounding_box_bottom/2).
+:- dynamic(parent/2).
+:- dynamic(npipes/1).
+:- dynamic(pipeNum/2).
+:- dynamic(sinkfd/2).
+:- dynamic(sourcefd/2).
