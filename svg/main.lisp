ad2b13ce03ed8e30abed5107ccdcd01fdbfb6d0e
Paul Tarvydas <paultarvydas@gmail.com>
2018-10-03 09:22:56 -0400

    baseline for svg to factbase

---
+++ svg/main.lisp
@@ -0,0 +1,20 @@
+;; test with Ken's output (already (list list)))
+
+(setf *default-pathname-defaults* #p"~/Dropbox/Projects/vsh/svg/")
+
+(defun main (str) 
+  (with-open-file (f (concatenate 'string str ".lisp") :direction :input)
+    (let ((list (read f)))
+      (assert (eq 'translate (first list)))
+      (let ((pair (second list)))
+	(assert pair)
+	(assert (listp pair))
+	(assert (= 2 (length pair)))
+	(assert (numberp (first pair)))
+	(assert (numberp (second pair)))
+	(convert-arrows (translate (first pair) (second pair) (third list)))))))
+
+(defun run (&optional (in-name "./sample") (out-name "./fb.pl"))
+  (with-open-file (f out-name :direction :output :if-does-not-exist :create :if-exists :supersede)
+      (tofb f (main in-name))))
+
