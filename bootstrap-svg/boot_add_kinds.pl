f3f37e66f5725e11e95eb3fcc7c0aee6a8eb2769
Paul Tarvydas <paultarvydas@gmail.com>
2018-12-09 13:18:42 -0500

    wip: working on boot_add_kinds and friends, to get FB containing same kinds of facts as for pl_vsh

---
+++ bootstrap-svg/boot_add_kinds.pl
@@ -0,0 +1,49 @@
+:- initialization(main).
+:- include('../common/head').
+
+main :-
+    readFB(user_input), 
+    forall(eltype(ID,box),createKinds(ID)),
+    writeFB,
+    halt.
+
+createKinds(Box) :-
+    write(user_error,'ck '),write(user_error,Box),nl(user_error),
+    text(Text,Str),
+    write(user_error,Text),write(user_error,' '),write(user_error,Str),write(user_error,' '),
+    textCompletelyInsideBox(Text,Box),
+    write(user_error,' '),write(user_error,Text),write(user_error,' '),
+    asserta(kind(Box,Str)).
+
+textCompletelyInsideBox(TextID,BoxID) :-
+    boundingboxCompletelyInside(TextID,BoxID).
+
+boundingboxCompletelyInside(ID1,ID2) :-
+    bounding_box_left(ID1,L1),
+    bounding_box_top(ID1,T1),
+    bounding_box_right(ID1,R1),
+    bounding_box_bottom(ID1,B1),
+
+    bounding_box_left(ID2,L2),
+    bounding_box_top(ID2,T2),
+    bounding_box_right(ID2,R2),
+    bounding_box_bottom(ID2,B2),
+
+write(user_error,'inside '), write(user_error,' '),
+write(user_error,L1), write(user_error,' '),
+write(user_error,T1), write(user_error,' '),
+write(user_error,R1), write(user_error,' '),
+write(user_error,B1), write(user_error,' '),
+write(user_error,L2), write(user_error,' '),
+write(user_error,T2), write(user_error,' '),
+write(user_error,R2), write(user_error,' '),
+write(user_error,B2), write(user_error,' '),
+nl(user_error),
+
+    L1 >= L2,
+    T1 >= T2,
+    R2 >= R1,
+    B2 >= B1.
+    
+
+:- include('../common/tail').
