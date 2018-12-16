fd8b6527b1c6c638d5a33f12c52f1213140d7651
Paul Tarvydas <paultarvydas@gmail.com>
2018-12-16 08:03:29 -0500

    wip - boot create centers

---
+++ bootstrap-svg/boot_create_centers.pl
@@ -0,0 +1,22 @@
+:- initialization(main).
+:- include('../common/head').
+
+main :-
+    readFB(user_input), 
+    forall(portName('nil',TextID),createCenter(TextID)),
+    forall(eltype(PortID,'port'),createCenter(PortID)),
+    writeFB,
+    halt.
+
+createCenter(ID) :-
+    bounding_box_left(ID,Left),
+    bounding_box_top(ID,Top),
+    bounding_box_right(ID,Right),
+    bounding_box_bottom(ID,Bottom),
+    X is Left + (Right / 2),
+    asserta(center_x(ID,X)),
+    Y is Top + (Bottom / 2),
+    asserta(center_y(ID,Y)).
+
+
+:- include('../common/tail').
