ce9f93993b3c9cea2b011b64a04c1ee37a604fd0
Paul Tarvydas <paultarvydas@gmail.com>
2018-12-17 14:47:18 -0500

    test4 - exploring comma vs. or-bar

--- bootstrap-svg/try.pl
+++ bootstrap-svg/try.pl
@@ -9,12 +9,15 @@ list_min([L|Ls], Min0, Min) :-
     list_min(Ls, Min1, Min).
 
 flatten([],[],[]).
-flatten([[N|ID]],[N],ID).
-%% flatten([[N1|I1]|Tail],[N1],I1,Tail).
+flatten([[N|ID]],[N],[ID]).
 %% IS is for eval'ing math.
 flatten([[N1|I1],[N2|I2]],N,I) :-
     unify(N,[N1,N2]),
     append(I1,I2,I).
+
+flatten([[N1,I1],[N2,I2],[N3,I3]],N,I) :-
+    unify(N,[N1,N2,N3]),
+    unify(I,[I1,I2,I3]).
 
 unify(X,X).
 
@@ -24,28 +27,14 @@ test1(N,I) :-
 test2(N,I) :-
     flatten([[1,id1]],N,I).
 
-test3(N,I,Tail) :-
-    flatten([[1,id1],[2,id2]],N,I,Tail).
-
-%% | ?- test3a(N,I).
+%% | ?- test3(N,I).
 %% I = [id1,id2]
 %% N = [1,2]
-test3a(N,I) :-
+test3(N,I) :-
     flatten([[1,id1],[2,id2]],N,I).
 
-%% (6 ms) yes
-%% | ?- test4(N,I,T).
-
-%% I = [id1]
-%% N = [1]
-%% T = [[2,id2],[3,id3]]
-
-%% (1 ms) yes
-
-test4(N,I,Tail) :-
-    flatten([[1,id1],[2,id2],[3,id3]],N,I,Tail).
-
-test5(N,I) :-
+test4(N,I) :-
     flatten([[1,id1],[2,id2],[3,id3]],N,I).
+
 
 
