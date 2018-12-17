a28c765bf52cfbf7fc59383267f7265605921ca0
Paul Tarvydas <paultarvydas@gmail.com>
2018-12-17 13:41:17 -0500

    wip - trying stuff out in try.pl ; test4 working

---
+++ bootstrap-svg/try.pl
@@ -0,0 +1,26 @@
+:- include('../common/head').
+
+list_min([L|Ls], Min) :-
+    list_min(Ls, L, Min).
+list_min([], Min, Min).
+list_min([L|Ls], Min0, Min) :-
+    Min1 is min(L, Min0),
+    list_min(Ls, Min1, Min).
+
+flatten([],[],[]).
+flatten([[N|ID]],[N],ID).
+flatten([[N1|I1]|Tail],[N1],I1,Tail).
+
+test1(N,I) :-
+    flatten([],N,I).
+
+test2(N,I) :-
+    flatten([[1,id1]],N,I).
+
+test3(N,I,Tail) :-
+    flatten([[1,id1],[2,id2]],N,I,Tail).
+
+test4(N,I,Tail) :-
+    flatten([[1,id1],[2,id2],[3,id3]],N,I,Tail).
+
+:- include('../common/tail').
