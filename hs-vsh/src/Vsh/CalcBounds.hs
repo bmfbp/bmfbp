3c6bcbf9483b4300d46a310da534c7cb3125c733
Kenneth Kan <kenhkan@gmail.com>
2018-12-25 21:49:50 -0500

    Refactor

--- hs-vsh/src/Vsh/CalcBounds.hs
+++ hs-vsh/src/Vsh/CalcBounds.hs
@@ -2,33 +2,21 @@ module Vsh.CalcBounds where
 
 import Prelude hiding (Left, Right)
 import Types.Diagram
-import qualified Data.List as DLT
+import qualified Data.List.Extra as DLE
 
 calcBounds :: Diagram -> Diagram
 calcBounds (Diagram facts) = Diagram (facts ++ newFacts)
   where
-    (geometryFacts, geometryNodeIds) =
-      unzip [ (fact, nodeId) | fact@(Geometry _ nodeId _) <- facts ]
-    nodeIds = DLT.nub geometryNodeIds
-    newFacts = DLT.concat $ DLT.map (boundingBoxFacts geometryFacts) nodeIds
-
-boundingBoxFacts :: [DiagramFact] -> NodeId -> [DiagramFact]
-boundingBoxFacts facts nodeId = newFacts
-  where
-    (x, y, w, h) = head
-      [ (x, y, width, height)
-        | (Geometry X nx x) <- facts
-        , (Geometry Y ny y) <- facts
-        , (Geometry Width nw width) <- facts
-        , (Geometry Height nh height) <- facts
-        , nx == nodeId
-        , ny == nodeId
-        , nw == nodeId
-        , nh == nodeId
-      ]
-    newFacts =
-      [ BoundingBox Left nodeId x
-      , BoundingBox Top nodeId y
-      , BoundingBox Right nodeId (x + w)
-      , BoundingBox Bottom nodeId (y + h)
+    newFacts = concat
+      [
+        [ BoundingBox Left nx x
+        , BoundingBox Top nx y
+        , BoundingBox Right nx (x + width)
+        , BoundingBox Bottom nx (y + height)
+        ]
+          | (Geometry X nx x) <- facts
+          , (Geometry Y ny y) <- facts
+          , (Geometry Width nw width) <- facts
+          , (Geometry Height nh height) <- facts
+          , DLE.allSame [nx, ny, nw, nh]
       ]
