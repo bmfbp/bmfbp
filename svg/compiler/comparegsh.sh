9ec990665e8be9d946a8e3941fb29be25eb687ea
Paul Tarvydas <paultarvydas@gmail.com>
2019-02-09 09:38:19 -0500

    fixed bug in comparegsh - Pipes should be Pipe

--- svg/compiler/comparegsh.sh
+++ svg/compiler/comparegsh.sh
@@ -4,8 +4,8 @@ base1=`basename $1`
 base2=`basename $2`
 f1=/tmp/${base1}
 f2=/tmp/${base2}
-sed -e 's/Pipes.*$/Pipes/' $1 >${f1}
-sed -e 's/Pipes.*$/Pipes/' $2 >${f2}
+sed -e 's/Pipe.*$/Pipe/' $1 >${f1}
+sed -e 's/Pipe.*$/Pipe/' $2 >${f2}
 echo
 if diff -q ${f1} ${f2}
 then echo OK
