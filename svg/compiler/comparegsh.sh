7760b8bc8a15f4f2967ee4727f1e8b13e57f6b94
Paul Tarvydas <paultarvydas@gmail.com>
2019-02-09 09:31:22 -0500

    comparegsh.sh needed sleep 1 to allow svgpasses to finish

--- svg/compiler/comparegsh.sh
+++ svg/compiler/comparegsh.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+sleep 1
 base1=`basename $1`
 base2=`basename $2`
 f1=/tmp/${base1}
 f2=/tmp/${base2}
 sed -e 's/Pipes.*$/Pipes/' $1 >${f1}
 sed -e 's/Pipes.*$/Pipes/' $2 >${f2}
-sleep 1
 echo
 if diff -q ${f1} ${f2}
 then echo OK
