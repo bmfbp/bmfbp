ab50d1386eb4c2db78ad2de612bf9ef08b0395f0
Paul Tarvydas <paultarvydas@gmail.com>
2019-02-13 16:08:01 -0500

    mods to comparegsh.sh

--- svg/compiler/comparegsh.sh
+++ svg/compiler/comparegsh.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-./normalizePipes.sh $1
-./normalizePipes.sh $2
+sleep 1
+base1=`basename $1`
+base2=`basename $2`
+f1=/tmp/${base1}
+f2=/tmp/${base2}
+sed -e 's/Pipe.*$/Pipe/' $1 >${f1}
+sed -e 's/Pipe.*$/Pipe/' $2 >${f2}
 echo
-if diff -q $1 $2
+if diff -q ${f1} ${f2}
 then echo OK
 else echo BAD
 fi
 echo
-
 
        
