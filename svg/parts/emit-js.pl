87df430e6213a70bf3ad918f93ce30fd83d94d64
Paul Tarvydas <paultarvydas@gmail.com>
2019-02-13 10:41:26 -0500

    wip: beginnings of js emitter

---
+++ svg/parts/emit-js.pl
@@ -0,0 +1,97 @@
+:- initialization(main).
+:- include('head').
+
+main :-
+    readFB(user_input),
+    write('{'),
+    nl,
+    write('  "name" : "'),
+    component(Name),
+    write(Name),
+    write('.js"'),
+    nl,
+    npipes(Npipes),
+    write('  "wirecount: " '),
+    write(Npipes),
+    write(","),
+    nl,
+    write('  "parts" : ['),
+    forall(kind(ID,_),emitComponent(ID)),
+    write('  ]'),
+    nl,
+    write('}'),
+    halt.
+
+inPipeP(P) :-
+    portName(P,0).
+
+inPipeP(P) :-
+    portName(P,in).
+
+outPipeP(P) :-
+    portName(P,out).
+
+outPipeP(P) :-
+    portName(P,1).
+
+errPipeP(P) :-
+    portName(P,err).
+
+errPipeP(P) :-
+    portName(P,2).
+
+writeIn(In) :-
+    writeSpaces,
+    inPipeP(In),!,
+    pipeNum(In,Pipe),
+    write('      "inPins" : [],'),
+    % write(' '),
+    % write(Pipe),
+    nl.
+
+writeOut(Out) :-
+    writeSpaces,
+    outPipeP(Out),!,
+    pipeNum(Out,Pipe),
+    write('      "outPins" : [],'),
+    % write(' '),
+    % write(Pipe),
+    nl.
+
+
+emitComponent(ID) :-
+    write('    {'),
+    nl,
+    forall(inputOfParent(ID,In),writeIn(In)),
+    forall(outputOfParent(ID,O),writeOut(O)),
+    writeSpaces,
+    writeExec,
+    write('      "exec": "'),
+    kind(ID,Name),
+    write(Name),
+    nl,
+    write('"'),
+    nl,
+    write('    },'),
+    nl.
+
+writeSpaces :- write('  ').
+
+inputOfParent(P,In) :-
+    parent(In,P),sink(_,In).
+
+outputOfParent(P,Out) :-
+    parent(Out,P),source(_,Out).
+    
+writeExec :-
+    write(exec),!.
+
+hasInput(ID) :-
+    eltype(ID,box),
+    parent(Port,ID),
+    eltype(Port,port),
+    sink(_,Port).
+
+
+:- include('tail').
+
