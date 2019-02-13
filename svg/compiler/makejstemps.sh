968f2a6334d84034388862f27f2f2d07f4475875
Paul Tarvydas <paultarvydas@gmail.com>
2019-02-13 17:38:24 -0500

    wip: makejstemps.sh

--- svg/compiler/makejstemps.sh
+++ svg/compiler/makejstemps.sh
@@ -13,8 +13,8 @@ calculate_distances <temp9.pro >temp10.pro
 assign_portnames <temp10.pro >temp11.pro
 mark_directions <temp11.pro >temp12.pro
 match_ports_to_components <temp12.pro >temp13a.pro
-sem_partsHaveSomePorts <temp13a.pro >temp13.pro
 assign_pipe_numbers_to_inputs <temp13.pro >temp14.pro
+sem_partsHaveSomePorts <temp13a.pro >temp13.pro
 assign_pipe_numbers_to_outputs <temp14.pro >temp15.pro
 assign_fds <temp15.pro >temp16.pro
 plsort <temp16.pro >temp17.pro
