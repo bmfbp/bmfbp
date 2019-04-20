72973f15b67b68d101aa8ba01acf39ec17b5b654
Paul Tarvydas <paultarvydas@gmail.com>
2019-04-20 14:26:43 -0400

    wip: working up to build-js, run-js is failing

--- svg/compiler/makejstemps.sh
+++ svg/compiler/makejstemps.sh
@@ -2,11 +2,6 @@
 set -v
 
 # scanner
-#hs-vsh-drawio-to-fb test1 <test1.svg >temp1.lisp
-#hs-vsh-drawio-to-fb test1 <test2.svg >temp1.lisp
-#hs-vsh-drawio-to-fb test1 <test2a.svg >temp1.lisp
-#hs-vsh-drawio-to-fb test1 <test3.svg >temp1.lisp
-#hs-vsh-drawio-to-fb test1 <test4.svg >temp1.lisp
 hs-vsh-drawio-to-fb test1 <test5.svg >temp1.lisp
 lib_insert_part_name svgc <temp1.lisp >temp2.lisp
 fb-to-prolog <temp2.lisp >temp3.pro
@@ -15,8 +10,9 @@ check_input <temp4.pro >temp5.pro
 
 # parser
 calc_bounds <temp5.pro >temp6.pro
+#add_kinds <temp6.pro >temp7.pro
 add_kinds <temp6.pro >temp6a.pro
-add_selfPorts <temp6a.pro >temp7.pro
+#add_selfPorts <temp6a.pro >temp7.pro
 make_unknown_port_names <temp7.pro >temp8.pro
 create_centers <temp8.pro >temp9.pro
 calculate_distances <temp9.pro >temp10.pro
