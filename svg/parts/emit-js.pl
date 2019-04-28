65d4fd7f38cba0e84c3d26809c7c9fbb8bd8e4d8
Paul Tarvydas <paultarvydas@gmail.com>
2019-04-28 10:44:43 -0400

    wip: modified emit-js.pl to include self

--- svg/parts/emit-js.pl
+++ svg/parts/emit-js.pl
@@ -14,11 +14,6 @@ main :-
     write('wirecount  '),
     write(Nwires),
     nl,
-%    write('self ('),
-%    nl,
-%    emitAllSelfPins,
-%    emitSelfExecs,
-%    write(' )'),
     write('parts ('),
     nl,
     emitAllPins,
@@ -26,9 +21,93 @@ main :-
     write(' )'),
     nl,
     write(')'),
+    nl,
     halt.
 
-:- include('children').
+emitAllPins :-
+    write('  ins ('), nl,
+    forall(selfInputPin(_,WireIndex),printSelfInputOrOutput(WireIndex)),
+    forall(eltype(PartID,box),getAllInPinsForPart(PartID)),
+    write('  )'), nl,
+    write('  outs ('), nl,
+    forall(selfOutputPin(_,WireIndex),printSelfInputOrOutput(WireIndex)),
+    forall(eltype(PartID,box),getAllOutPinsForPart(PartID)),
+    write('  )'), nl.
+
+printSelfInputOrOutput(Pin) :-
+    write('    (self nil '),
+    wireIndex(Pin,WireIndex),
+    write(WireIndex),
+    write(' '),
+    write(Pin),
+    write(')'),
+    nl.
+    
+getAllInPinsForPart(PartID):-
+    forall(inPinOfPart(PortID,PartID),getOneInPin(PortID,PartID)).
+
+getAllInPinsForPart(PartID):-
+    forall(inPinOfPart(PortID,PartID),getOneInPin(PortID,PartID)).
+
+inPinOfPart(PortID,PartID) :-
+    parent(PortID,PartID),
+    sink(_,PortID).
+
+getOneInPin(PortID,PartID):-
+    eltype(PortID,port),
+    wireNum(PortID,Wire),
+    portIndex(PortID,Pin),
+    write('    ('),
+    write(PartID),
+    write(' '),
+    write(PortID),
+    write(' '),
+    write(Wire),
+    write(' '),
+    write(Pin),
+    write(')'),
+    nl.
+
+getAllOutPinsForPart(PartID):-
+    forall(outPinOfPart(PortID,PartID),getOneOutPin(PortID,PartID)).
+
+outPinOfPart(PortID,PartID) :-
+    parent(PortID,PartID),
+    source(_,PortID).
+
+getOneOutPin(PortID,PartID):-
+    eltype(PortID,port),
+    wireNum(PortID,Wire),
+    portIndex(PortID,Pin),
+    write('    ('),
+    write(PartID),
+    write(' '),
+    write(PortID),
+    write(' '),
+    write(Wire),
+    write(' '),
+    write(Pin),
+    write(')'),
+    nl.
+
+
+emitExecs:-
+    write('  execs ('),
+    nl,
+    forall(kind(PartID,ExecName),emitExec(PartID,ExecName)),
+    write('    (self "schematic")'),
+    nl,
+    write('  )'),
+    nl.
+
+emitExec(PartID,ExecName) :-
+    write('    ('),
+    write(PartID),
+    write(' "'),
+    write(ExecName),
+    write('")'),
+    nl.
+
 :- include('tail').
 
 
