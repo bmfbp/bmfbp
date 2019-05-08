b0dd3b899be0a8e17dfc385dd7b8cadeca860e76
Paul Tarvydas <paultarvydas@gmail.com>
2019-05-08 18:15:24 -0400

    wip: finished conversion to portIntersection

--- svg/parts/match_ports_to_components.pl
+++ svg/parts/match_ports_to_components.pl
@@ -35,41 +35,18 @@ assign_parent_for_port(PortID) :-
     asserta(n_c(PortID)),
     nle,nle,we('no parent box for port '),we(PortID),nle,nle,nle.
 
+portIntersection(PortID,ParentID):-
+    leftTopPointCompletelyInsideBoundingBox(PortID,ParentID).
 
 portIntersection(PortID,ParentID):-
-    bounding_box_left(PortID, Left),
-    bounding_box_top(PortID, Top),
-    bounding_box_right(PortID, Right),
-    bounding_box_bottom(PortID, Bottom),
-    bounding_box_left(ParentID, PLeft),
-    bounding_box_top(ParentID, PTop),
-    bounding_box_right(ParentID, PRight),
-    bounding_box_bottom(ParentID, PBottom),
-    intersects(Left, Top, Right, Bottom, PLeft, PTop, PRight, PBottom).
+    topRightPointCompletelyInsideBoundingBox(PortID,ParentID).
 
-intersects(PortLeft,_,_,PortBottom,ParentLeft, ParentTop, ParentRight, ParentBottom) :-
-    PortLeft >= ParentLeft,
-    PortLeft =< ParentRight,
-    PortBottom =< ParentBottom,
-    PortBottom >= ParentTop.
+portIntersection(PortID,ParentID):-
+    rightBottomPointCompletelyInsideBoundingBox(PortID,ParentID).
 
-intersects(PortLeft,PortTop,_,_,ParentLeft, ParentTop, ParentRight, ParentBottom) :-
-    PortLeft >= ParentLeft,
-    PortLeft =< ParentRight,
-    PortTop >= ParentTop,
-    PortTop =< ParentBottom.
+portIntersection(PortID,ParentID):-
+    bottomLeftPointCompletelyInsideBoundingBox(PortID,ParentID).
 
-intersects(_,_,PortRight,PortBottom,ParentLeft, ParentTop, ParentRight, ParentBottom) :-
-    PortRight >= ParentLeft,
-    PortRight =< ParentRight,
-    PortBottom >= ParentTop,
-    PortBottom =< ParentBottom.
-
-intersects(_,PortTop,PortRight,_,ParentLeft, ParentTop, ParentRight, ParentBottom) :-
-    PortRight >= ParentLeft,
-    PortRight =< ParentRight,
-    PortTop >= ParentTop,
-    PortTop =< ParentBottom.
 
 :- include('tail').
 
