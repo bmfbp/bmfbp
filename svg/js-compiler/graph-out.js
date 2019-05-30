bee1667f62fb1875e1d6ce1f29c4d0f3f0f32b2f
Paul Tarvydas <paultarvydas@gmail.com>
2019-05-30 16:45:10 -0400

    wip: added compare-graph.sh ; convert strings to unique ids but they are not unconverted (yet)

---
+++ svg/js-compiler/graph-out.js
@@ -0,0 +1,46 @@
+{
+  "name" : "js_test_emitter.js",
+  "wirecount" : 3,
+  "self" : {
+      "partName" : "SELF",
+      "in-count" : 0,
+      "out-count" : 0,
+      "inPins" : [],
+      "outPins" : [],
+      "exec" : "schematic"
+    }
+  "parts" : [
+    {
+      "partName" : "ID369",
+      "in-count" : 1,
+      "out-count" : 0,
+      "inPins" : [[0]],
+      "outPins" : [],
+      "exec" : "struidG371"
+    },
+    {
+      "partName" : "ID374",
+      "in-count" : 1,
+      "out-count" : 2,
+      "inPins" : [[1]],
+      "outPins" : [[],[0]],
+      "exec" : "struidG376"
+    },
+    {
+      "partName" : "ID382",
+      "in-count" : 1,
+      "out-count" : 2,
+      "inPins" : [[2]],
+      "outPins" : [[],[1]],
+      "exec" : "struidG384"
+    },
+    {
+      "partName" : "ID392",
+      "in-count" : 0,
+      "out-count" : 2,
+      "inPins" : [],
+      "outPins" : [[],[2]],
+      "exec" : "struidG394"
+    }
+  ]
+}
