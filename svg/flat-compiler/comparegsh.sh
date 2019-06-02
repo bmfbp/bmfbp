ece28d3e087da7fd3c705296f7b59453505309e2
Paul Tarvydas <paultarvydas@gmail.com>
2019-06-02 08:43:39 -0400

    comparegsh dumbed down (due to string mangling) but works for flat-compiler

--- svg/flat-compiler/comparegsh.sh
+++ svg/flat-compiler/comparegsh.sh
@@ -4,8 +4,8 @@ base1=`basename $1`
 base2=`basename $2`
 f1=/tmp/${base1}
 f2=/tmp/${base2}
-sed -e 's/Pipe.*$/Pipe/' $1 >${f1}
-sed -e 's/Pipe.*$/Pipe/' $2 >${f2}
+sed -e 's/Pipe.*$/Pipe/' $1 | sed -e 's/exec.*$/exec/' >${f1}
+sed -e 's/Pipe.*$/Pipe/' $2 | sed -e 's/exec.*$/exec/' >${f2}
 echo
 if diff -q ${f1} ${f2}
 then echo OK
