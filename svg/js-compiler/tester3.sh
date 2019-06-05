ae53707a34c6579628c44422708dab9a781b7bef
Paul Tarvydas <paultarvydas@gmail.com>
2019-06-05 08:22:20 -0400

    tester3.sh is used only for debug

--- svg/js-compiler/tester3.sh
+++ svg/js-compiler/tester3.sh
@@ -9,32 +9,31 @@ fb_to_prolog $NAME <temp2.lisp >temp3.pro
 sort <temp3.pro >temp4.pro
 check_input $NAME <temp4.pro >temp5.pro
 
+calc_bounds $NAME <temp5.pro >temp6.pro
+find_comments $NAME <temp6.pro >temp6a.pro
 
-# calc_bounds $NAME <temp5.pro >temp6.pro
-# find_comments $NAME <temp6.pro >temp6a.pro
+# rest of parser
+add_kinds $NAME <temp6a.pro >temp7.pro
+make_unknown_port_NAMEs $NAME <temp7.pro >temp8.pro
+create_centers $NAME <temp8.pro >temp9.pro
+calculate_distances $NAME <temp9.pro >temp10.pro
+assign_portnames $NAME <temp10.pro >temp11.pro
+markIndexedPorts $NAME <temp11.pro >temp12.pro
+coincidentPorts $NAME <temp12.pro >temp13.pro
+mark_directions $NAME <temp13.pro >temp14.pro
+match_ports_to_components <temp14.pro >temp15.pro
 
-# # rest of parser
-# add_kinds $NAME <temp6a.pro >temp7.pro
-# make_unknown_port_NAMEs $NAME <temp7.pro >temp8.pro
-# create_centers $NAME <temp8.pro >temp9.pro
-# calculate_distances $NAME <temp9.pro >temp10.pro
-# assign_portnames $NAME <temp10.pro >temp11.pro
-# markIndexedPorts $NAME <temp11.pro >temp12.pro
-# coincidentPorts $NAME <temp12.pro >temp13.pro
-# mark_directions $NAME <temp13.pro >temp14.pro
-# match_ports_to_components <temp14.pro >temp15.pro
+# semantics
+sem_partsHaveSomePorts <temp15.pro >temp16.pro
+sem_allPortsHaveAnIndex <temp16.pro >temp17.pro
 
-# # semantics
-# sem_partsHaveSomePorts <temp15.pro >temp16.pro
-# sem_allPortsHaveAnIndex <temp16.pro >temp17.pro
-
-# # emitter
-# assign_wire_numbers_to_inputs $NAME <temp17.pro >temp18.pro
-# assign_wire_numbers_to_outputs $NAME <temp18.pro >temp19.pro
-# assign_portIndices $NAME <temp19.pro >temp20.pro
-# emit_js $NAME <temp20.pro >temp21a.lisp
-# unmap-strings $NAME <temp21a.lisp >temp21.lisp
-# emit_js2 $NAME <temp21.lisp >temp22.js
+# emitter
+assign_wire_numbers_to_inputs $NAME <temp17.pro >temp18.pro
+assign_wire_numbers_to_outputs $NAME <temp18.pro >temp19.pro
+assign_portIndices $NAME <temp19.pro >temp20.pro
+emit_js $NAME <temp20.pro >temp21a.lisp
+unmap-strings $NAME <temp21a.lisp >temp21.lisp
+emit_js2 $NAME <temp21.lisp >temp22.js
 
 
 
