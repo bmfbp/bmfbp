c6e2a6c5df4dc9070d10cff8d75125914bd34f19
Paul Tarvydas <paultarvydas@gmail.com>
2019-06-13 15:35:15 -0400

    merged hs fix and modified create-text-objects.lisp to fit

--- svg/parts/create-text-objects.lisp
+++ svg/parts/create-text-objects.lisp
@@ -17,49 +17,51 @@
 
 (defun create-text-objects (list)
   (assert (listp list))
-  ;(format *error-output* "list: ~S~%" list)
-  (case (car list)
-    
-    (translate
-     (flet ((failure () (die (format nil "badly formed translate /~A/~%" list))))
-       (let ((pair (second list))
-             (tail (third list)))
+					;(format *error-output* "list: ~S~%" list)
+  (if (listp (car list))
+      (create-text-objects (car list))
+      (case (car list)
+	
+	(translate
+	 (flet ((failure () (die (format nil "badly formed translate /~A/~%" list))))
+	   (let ((pair (second list))
+		 (tail (third list)))
 
-	 (when (and
-		(= 1 (length tail))
-		(listp (first tail))
-		(stringp (first (first tail)))
-		(null  (second (first tail))))
-	   (format *error-output* "~%~%fixed bug2 in create-text-objects.lisp~%~%")
-	   (setf tail (first tail)))
-	 
-	 (cond ((list-of-lists-p tail)
-                `(translate ,pair ,(mapcar #'create-text-objects tail)))
+	     (when (and
+		    (= 1 (length tail))
+		    (listp (first tail))
+		    (stringp (first (first tail)))
+		    (null  (second (first tail))))
+	       (format *error-output* "~%~%fixed bug2 in create-text-objects.lisp~%~%")
+	       (setf tail (first tail)))
+	     
+	     (cond ((list-of-lists-p tail)
+                    `(translate ,pair ,(mapcar #'create-text-objects tail)))
 
-               ((matches-text-item-p tail)
-                ;; (translate (N M) ("emit")) --> ((translate (N M) ((text "emit" 0 0 w/2 h))))
-                (let ((text (text-part tail)))
-                  (let ((half-width (/ (* (length text) *default-font-width*) 2)))
-                    `((translate ,pair
-                                 ((text ,text 
-                                        0
-                                        0
-                                        ,half-width
-                                        ,*default-font-height*)))))))
+		   ((matches-text-item-p tail)
+                    ;; (translate (N M) ("emit")) --> ((translate (N M) ((text "emit" 0 0 w/2 h))))
+                    (let ((text (text-part tail)))
+                      (let ((half-width (/ (* (length text) *default-font-width*) 2)))
+			`((translate ,pair
+                                     ((text ,text 
+                                            0
+                                            0
+                                            ,half-width
+                                            ,*default-font-height*)))))))
 
-               (t (failure))))))
-    
-    ((line rect component ellipse dot speechbubble)
-     list)
+		   (t (failure))))))
+	
+	((line rect component ellipse dot speechbubble)
+	 list)
 
-    (metadata
-     (if (and 
-	  (= 2 (length list)) 
-	  (stringp (second list)))
-	 ;; (metadata "[lotsofstrings]") --> (metadata strid 0 0 w/2 h)
-	 (let ((half-width (/ (* (get-metadata-len (second list)) *default-font-width*) 2)))
-	   `(metadata ,(second list) 0 0 ,half-width ,*default-font-height*))
-	 (die (format nil "badly formed metadata /~S/~%" list))))
-    
-    (otherwise
-     (die (format nil "~%bad format in create-text-objects /~S/~%" list)))))
+	(metadata
+	 (if (and 
+	      (= 2 (length list)) 
+	      (stringp (second list)))
+	     ;; (metadata "[lotsofstrings]") --> (metadata strid 0 0 w/2 h)
+	     (let ((half-width (/ (* (get-metadata-len (second list)) *default-font-width*) 2)))
+	       `(metadata ,(second list) 0 0 ,half-width ,*default-font-height*))
+	     (die (format nil "badly formed metadata /~S/~%" list))))
+	
+	(otherwise
+	 (die (format nil "~%bad format in create-text-objects /~S/~%" list))))))
