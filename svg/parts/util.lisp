39bc72c6a1b05331c1c4d135ce7d71239d57f09a
Paul Tarvydas <paultarvydas@gmail.com>
2019-06-19 18:17:34 -0400

    playing with sed command

--- svg/parts/util.lisp
+++ svg/parts/util.lisp
@@ -19,13 +19,17 @@
 (defun init-string-map ()
   (setf *string-map* (make-hash-table)))
 
+(defun @escape-strings-for-sed (s)
+  s)
+
 (defun write-string-map (fname sed-fname)
   (with-open-file (f fname :direction :output :if-exists :supersede)
     (with-open-file (sed sed-fname :direction :output :if-exists :supersede)
       (maphash
        #'(lambda (k v)
 	   (format f "(~A ~S)~%" k v)
-	   (format sed "s/ ~A/[~A]/g~%" k v))
+	   (let ((str (@escape-strings-for-sed v)))
+	     (format sed "s/ ~A/[~A]/g~%" k str)))
        *string-map*)
       (format sed "s/] /]./g~%")
       (format sed "s/null null null/N.C./g~%")
