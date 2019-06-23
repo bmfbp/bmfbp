0b9fc73153ad24a5002dbd7d35f910d260f0c1e0
Paul Tarvydas <paultarvydas@gmail.com>
2019-06-23 16:43:20 -0400

    promoted tee to makefile with regression

---
+++ svg/js-compiler/tee_regression.js
@@ -0,0 +1,15 @@
+{
+  "name" : "tee.js",
+  "wirecount" : 3,
+  "self" : {
+      "partName" : "SELF",
+      "in-count" : 1,
+      "out-count" : 3,
+      "inPins" : [[0,0]],
+      "outPins" : [[0],[1],[2]],
+      "exec" : "schematic"
+    }
+  "parts" : [
+
+  ]
+}
