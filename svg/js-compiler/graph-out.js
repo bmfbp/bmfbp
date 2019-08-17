7c7c2b50569b1b2c1faf6c81e51dc76a8cc5a26d
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-17 12:20:19 -0400

    rewrote graph-out.js due to changes like inCount

--- svg/js-compiler/graph-out.js
+++ svg/js-compiler/graph-out.js
@@ -3,44 +3,44 @@
   "wirecount" : 3,
   "self" : {
       "partName" : "SELF",
-      "in-count" : 0,
-      "out-count" : 0,
+      "inCount" : 0,
+      "outCount" : 0,
       "inPins" : [],
       "outPins" : [],
-      "exec" : "schematic"
+      "kindName" : "schematic"
     }
   "parts" : [
     {
       "partName" : "ID369",
-      "in-count" : 1,
-      "out-count" : 0,
+      "inCount" : 1,
+      "outCount" : 0,
       "inPins" : [[0]],
       "outPins" : [],
-      "exec" : "emit"
+      "kindName" : "emit"
     },
     {
       "partName" : "ID374",
-      "in-count" : 1,
-      "out-count" : 2,
+      "inCount" : 1,
+      "outCount" : 2,
       "inPins" : [[1]],
       "outPins" : [[],[0]],
-      "exec" : "assign_portIndices"
+      "kindName" : "assign_portIndices"
     },
     {
       "partName" : "ID382",
-      "in-count" : 1,
-      "out-count" : 2,
+      "inCount" : 1,
+      "outCount" : 2,
       "inPins" : [[2]],
       "outPins" : [[],[1]],
-      "exec" : "assign_wire_numbers_to_outputs"
+      "kindName" : "assign_wire_numbers_to_outputs"
     },
     {
       "partName" : "ID392",
-      "in-count" : 0,
-      "out-count" : 2,
+      "inCount" : 0,
+      "outCount" : 2,
       "inPins" : [],
       "outPins" : [[],[2]],
-      "exec" : "assign_wire_numbers_to_inputs"
+      "kindName" : "assign_wire_numbers_to_inputs"
     }
   ]
 }
