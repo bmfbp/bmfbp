d212df90b5ea1e1decfd07e336ee19cd1d018a2f
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-18 09:03:56 -0400

    script modified to quit before SBCL if an fATAL errors

--- svg/parts/sem_allPortsHaveAnIndex.pl
+++ svg/parts/sem_allPortsHaveAnIndex.pl
@@ -12,7 +12,7 @@ check_has_index(PortID):-
     portIndex(PortID,Second),
     First =\= Second,
     !,
-    asserta(log('eRROR',PortID,'has_more_than_one_index',First,Second,'allPortsHaveAnIndex')).
+    asserta(log('fATAL',PortID,'has_more_than_one_index',First,Second,'allPortsHaveAnIndex')).
 
 check_has_index(PortID):-
     portIndex(PortID,_),!.
@@ -21,7 +21,7 @@ check_has_index(PortID):-
     n_c(PortID).
 
 check_has_index(PortID):-
-    asserta(log('eRROR',PortID,'has_no_index','allPortsHaveAnIndex')).
+    asserta(log('fATAL',PortID,'has_no_index','allPortsHaveAnIndex')).
 
 :- include('tail').
 
