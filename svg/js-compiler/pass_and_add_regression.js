8c3a3a5b81137f1dbee7620532298f84e262d353
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-20 17:41:20 -0400

    rewrote regression tests

--- svg/js-compiler/pass_and_add_regression.js
+++ svg/js-compiler/pass_and_add_regression.js
@@ -4,19 +4,19 @@
   "metadata" : [{"dir":"test_cases/leaves","kindName":"add_1","repo":"https://github.com/bmfbp/bmfbp.git","file":"add_1.js","ref":"master"}],
   "self" : {
       "partName" : "SELF",
-      "inCount" : 3,
-      "outCount" : 1,
-      "inPins" : [[0],[1],[3]],
-      "outPins" : [[0,0]],
+      "inCount" : 2,
+      "outCount" : 0,
+      "inPins" : [[1],[2]],
+      "outPins" : [],
       "kindName" : "schematic"
     }
   "parts" : [
     {
       "partName" : "ID385",
-      "inCount" : 1,
-      "outCount" : 1,
-      "inPins" : [[1]],
-      "outPins" : [[2]],
+      "inCount" : 0,
+      "outCount" : 0,
+      "inPins" : [],
+      "outPins" : [],
       "kindName" : "add_1"
     }
   ]
