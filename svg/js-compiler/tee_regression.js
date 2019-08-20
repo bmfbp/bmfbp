0256a484bd1362143c02f68c4fefee5ce022835c
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-20 17:45:54 -0400

    updated regression tests

---
+++ svg/js-compiler/tee_regression.js
@@ -0,0 +1,15 @@
+{
+  "name" : "tee.js",
+  "wirecount" : 3,
+  "self" : {
+      "partName" : "SELF",
+      "inCount" : 0,
+      "outCount" : 2,
+      "inPins" : [],
+      "outPins" : [[1],[1]],
+      "kindName" : "schematic"
+    }
+  "parts" : [
+
+  ]
+}
