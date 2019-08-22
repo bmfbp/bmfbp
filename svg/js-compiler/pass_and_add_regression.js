0b242ae1bf6e5d93f7bd3b81ac6d8726d9b39eca
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-22 14:33:45 -0400

    rewrote regressions

--- svg/js-compiler/pass_and_add_regression.js
+++ svg/js-compiler/pass_and_add_regression.js
@@ -1,22 +1,22 @@
 {
-  "name" : "pass_and_add.js",
+  "name" : "pass_and_add",
   "wirecount" : 4,
   "metadata" : [{"dir":"test_cases/leaves","kindName":"add_1","repo":"https://github.com/bmfbp/bmfbp.git","file":"add_1.js","ref":"master"}],
   "self" : {
       "partName" : "SELF",
-      "inCount" : 2,
-      "outCount" : 0,
-      "inPins" : [[1],[2]],
-      "outPins" : [],
+      "inCount" : 3,
+      "outCount" : 1,
+      "inPins" : [[1],[2],[0]],
+      "outPins" : [[1,1]],
       "kindName" : "schematic"
     }
   "parts" : [
     {
       "partName" : "ID385",
-      "inCount" : 0,
-      "outCount" : 0,
-      "inPins" : [],
-      "outPins" : [],
+      "inCount" : 1,
+      "outCount" : 1,
+      "inPins" : [[2]],
+      "outPins" : [[1]],
       "kindName" : "add_1"
     }
   ]
