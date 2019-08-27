b8f8b4593eb89bc66e1c2ade171c837ca2c2a692
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-27 14:50:55 -0400

    folded macros and classes back into emit_js4

--- svg/parts/emit-classes.lisp
+++
@@ -1,4 +0,0 @@
-(defclass part-descriptor-class ()
-  ((sinks :accessor sinks :initform (new-string-hashmap))
-   (sources :accessor sources :initform (new-string-hashmap))
-   (id :accessor id :initform nil)))
--- svg/parts/emit_js4.lisp
+++ svg/parts/emit_js4.lisp
@@ -1,3 +1,22 @@
+(defmacro @loop (&body body) `(loop ,@body))
+(defmacro @exit-when (expr) `(when ,expr (return)))
+
+(defmacro new-string-hashmap () `(make-hash-table :test 'equal))
+
+(defmacro source-name (w) `(if (eq 'NULL (second ,w)) "self" (second ,w))) ;; part
+(defmacro source-pin-name (w) `(third ,w)) ;; pin
+(defmacro sink-name (w) `(if (eq 'NULL (seventh ,w)) "self" (seventh ,w))) ;; part
+(defmacro sink-pin-name (w) `(eighth ,w)) ;; pin
+(defmacro source-id (w) `(first ,w))
+(defmacro sink-id (w) `(sixth ,w))
+(defmacro wire-number (w) `(fifth ,w))
+
+(defclass part-descriptor-class ()
+  ((sinks :accessor sinks :initform (new-string-hashmap))
+   (sources :accessor sources :initform (new-string-hashmap))
+   (id :accessor id :initform nil)))
+
+
 (defun main ()
   (with-open-file (t27 "~/projects/bmfbp/svg/js-compiler/temp27.lisp" :direction :input)
     (let ((whole (read t27))
