2c813bc2037cd6ffd5271076dcc87b34b6d434ed
Paul Tarvydas <guitarvydas@gmail.com>
2019-09-14 17:40:37 -0400

    created loadall.lisp and split code off to bounding-boxes.lisp

---
+++ svg/lisp-parts/loadall.lisp
@@ -0,0 +1,6 @@
+(ql:quickload :paip-prolog)
+(ql:quickload :loops)
+
+;(load "find-comments")
+(load "~/projects/bmfbp/svg/lisp-parts/bounding-boxes")
+(load "~/projects/bmfbp/svg/lisp-parts/lwpasses")
