257345ad2021703ef9e280905b49e743759bd375
Paul Tarvydas <guitarvydas@gmail.com>
2019-09-14 17:15:39 -0400

    moved fatal error to a different rule

--- svg/parts/find_comments.pl
+++ svg/parts/find_comments.pl
@@ -10,15 +10,16 @@ main :-
 condComment :-
     forall(speechbubble(ID),createComments(ID)).
 
-condComment :-
-    asserta(log('fATAL',commentFinderFailed)),
-    true.
-
 createComments(BubbleID) :-
     text(TextID,_),
     textCompletelyInsideBox(TextID,BubbleID),
+    !,
     asserta(used(TextID)),
     asserta(comment(TextID)).
+
+createComments(_) :-
+    asserta(log('fATAL',commentFinderFailed)),
+    true.
 
 textCompletelyInsideBox(TextID,BubbleID) :-
     pointCompletelyInsideBoundingBox(TextID,BubbleID).
