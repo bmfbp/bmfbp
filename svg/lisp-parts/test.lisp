e6ce287359bfa4ffd1960c31b11310e3f189aeeb
Paul Tarvydas <guitarvydas@gmail.com>
2019-09-18 08:44:49 -0400

    test.lisp is an example of a query that gives two results instead of 1

---
+++ svg/lisp-parts/test.lisp
@@ -0,0 +1,3 @@
+(paiprolog:prolog-collect (?T ?S) (speechbubble ?S) (write d) (text ?T ?) (write e) (point-completely-inside-bounding-box ?T ?S) (write f))
+
+(paiprolog:prolog-collect (?T ?S) (speechbubble ?S) (write a) (text ?T ?) (write b) (text-completely-inside-box ?T ?S) (write c))
