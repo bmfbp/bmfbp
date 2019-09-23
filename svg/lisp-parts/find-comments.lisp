dcb1681447ae51c527fd3649691ed5694af489fa
Paul Tarvydas <guitarvydas@gmail.com>
2019-09-22 20:43:46 -0400

    wip: working on bounding boxes, git wants checkin of find-comments

--- svg/lisp-parts/find-comments.lisp
+++ svg/lisp-parts/find-comments.lisp
@@ -1,4 +1,6 @@
-(<- (find-and-assert-comments-query)
+(in-package :arrowgram)
+
+(paip::<- (find-and-assert-comments-query)
 (lisp ?junk1 (format *error-output* "~&in find-and-assert-comments-query 1~%"))
     (speechbubble ?bid)
 (lisp ?junk2 (format *error-output* "~&in find-and-assert-comments-query 2~%"))
@@ -7,19 +9,19 @@
     (textCompletelyInsideBox ?textid ?bid)
 (lisp ?junk4 (format *error-output* "~&in find-and-assert-comments-query 4~%"))
     !
-    (add-clause `((used ,textid)))
-    (add-clause `((comment ,textid))))
+    (paip::add-clause `((used ,textid)))
+    (paip::add-clause `((comment ,textid))))
 
-(<- (find-and-assert-comments-query)
+(paip::<- (find-and-assert-comments-query)
 (lisp ?junk5 (format *error-output* "~&in find-and-assert-comments-query fatal~%"))
-    (add-clause `((log "FATAL error in find-comments"))))
+    (paip::add-clause `((log "FATAL error in find-comments"))))
 
-(<- (textCompletelyInsideBox ?TextID ?BubbleID)
+(paip::<- (textCompletelyInsideBox ?TextID ?BubbleID)
     (pointCompletelyInsideBoundingBox ?TextID ?BubbleID))
 
 
 
 (defun find-comments ()
 (format *error-output* "~&starting find-and-assert-comments-query~%")
-  (prove-all '((find-and-assert-comments-query)) no-bindings)
+  (paip::prove-all '((find-and-assert-comments-query)) paip::no-bindings)
 (format *error-output* "~&ending find-and-assert-comments-query~%"))
