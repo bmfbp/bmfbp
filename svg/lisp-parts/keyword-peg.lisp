444bb5cdf70a514f002af216b80c5fee7fbab648
Paul Tarvydas <guitarvydas@gmail.com>
2019-10-14 08:42:19 -0400

    added user_input

--- svg/lisp-parts/keyword-peg.lisp
+++ svg/lisp-parts/keyword-peg.lisp
@@ -3,13 +3,16 @@
 
 ;; not all of these are Prolog keywords, some are just convenience matches for
 ;; the working code base
-(peg:rule prolog::Keyword "pTrue / pFail / pHalt / pIs / pAsserta / pRetract / pReadFB / pWriteFB / pInclude / pInitialization / pGAssign / pForall / pColonDash / pNotSame / pNot / pNotSame / pSame / pNotUnifySame / pUnifySame / pGreaterEqual / pLessEqual / pCut / pPeriod / pComma / pLpar / pRpar / pWriteterm / pWrite / pNl / pUserError / pCurrentInput / pGRead / pMinus / pPlus / pAsterisk / pSlash"
+(peg:rule prolog::Keyword "pTrue / pFail / pHalt / pIs / pAsserta / pRetract / pReadFB / pWriteFB / pInclude / pInitialization / pGAssign / pForall / pColonDash / pNotSame / pNot / pNotSame / pSame / pNotUnifySame / pUnifySame / pGreaterEqual / pLessEqual / pCut / pPeriod / pComma / pLpar / pRpar / pWriteterm / pWrite / pNl / pUserError / pCurrentInput / pUserInput / pGRead / pMinus / pPlus / pAsterisk / pSlash"
   (:lambda (x) x))
 
 (peg:rule prolog::pUserError "'user_error' Spacing"
   (:lambda (x) (declare (ignore x))))
 
 (peg:rule prolog::pCurrentInput "'current_input' Spacing"
+  (:lambda (x) (declare (ignore x))))
+
+(peg:rule prolog::pUserInput "'user_input' Spacing"
   (:lambda (x) (declare (ignore x))))
 
 (peg:rule prolog::pGRead "'g_read' Spacing"
