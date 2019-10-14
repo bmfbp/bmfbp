3f9fdc08f5c4b726fa0fb37eb85f62cfeab4ce0b
Paul Tarvydas <guitarvydas@gmail.com>
2019-10-14 09:33:30 -0400

    remove duplicate match

--- svg/lisp-parts/keyword-peg.lisp
+++ svg/lisp-parts/keyword-peg.lisp
@@ -3,7 +3,7 @@
 
 ;; not all of these are Prolog keywords, some are just convenience matches for
 ;; the working code base
-(peg:rule prolog::Keyword "pTrue / pFail / pHalt / pIs / pAsserta / pRetract / pReadFB / pWriteFB / pInclude / pInitialization / pGAssign / pForall / pColonDash / pNotSame / pNot / pNotSame / pSame / pNotUnifySame / pUnifySame / pGreaterEqual / pLessEqual / pCut / pPeriod / pComma / pLpar / pRpar / pWriteterm / pWrite / pNl / pUserError / pCurrentInput / pUserInput / pGRead / pMinus / pPlus / pAsterisk / pSlash"
+(peg:rule prolog::Keyword "pTrue / pFail / pHalt / pIs / pAsserta / pRetract / pReadFB / pWriteFB / pInclude / pInitialization / pGAssign / pForall / pColonDash / pNot / pNotSame / pSame / pNotUnifySame / pUnifySame / pGreaterEqual / pLessEqual / pCut / pPeriod / pComma / pLpar / pRpar / pWriteterm / pWrite / pNl / pUserError / pCurrentInput / pUserInput / pGRead / pMinus / pPlus / pAsterisk / pSlash"
   (:lambda (x) x))
 
 (peg:rule prolog::pUserError "'user_error' Spacing"
