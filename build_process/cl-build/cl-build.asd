60adb4b8ab2d510460b9651d9ef37cb2726f4721
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-16 12:12:00 -0500

    first light on a simple pipeline

---
+++ build_process/cl-build/cl-build.asd
@@ -0,0 +1,10 @@
+(defsystem "cl-build"
+  :depends-on (:loops :cl-event-passing)
+  :around-compile (lambda (next)
+                    (proclaim '(optimize (debug 3)
+                                         (safety 3)
+                                         (speed 0)))
+                    (funcall next))
+  :components ((:module "source"
+                        :pathname "./"
+                        :components ((:file "convert")))))
---
+++ build_process/cl-build/convert.lisp
@@ -0,0 +1,29 @@
+(in-package :cl-event-passing-user)
+
+(defun convert-JSON-to-Lisp-network (input-stream output-stream)
+  (@initialize)
+  (let ((top-level (@new-schematic :input-pins '(:in) :output-pins '(:out)))
+        (cat (@new-code :input-handler #'cat :input-pins '(:in) :output-pins '(:out)))
+        (wire-in (@new-wire))
+        (wire-out (@new-wire))
+        (char 'EOF))
+    (@top-level-schematic top-level)
+    (@add-part-to-schematic top-level cat)
+    (@add-inbound-receiver-to-wire wire-in cat :in)
+    (@add-outbound-receiver-to-wire wire-out cat :out)
+    (@add-source-to-schematic top-level top-level :in wire-in)
+    (@add-source-to-schematic top-level cat :out wire-out)
+
+    (@:loop
+     (setf char (read-char input-stream nil 'EOF))
+     (@:exit-when (eq 'EOF char))
+     (let ((result (cat char)))
+       (format output-stream "~&~S" result)))))
+
+(defun main ()
+  (with-open-file (in (asdf:system-relative-pathname :cl "junk.json") :direction :input)
+    (convert-JSON-to-Lisp-network in *standard-output*)))
+
+(defun cat (lisp-obj)
+  (format nil "count=~a code=0x~x char=~S~%" (length (string lisp-obj)) (char-code lisp-obj) lisp-obj))
+
