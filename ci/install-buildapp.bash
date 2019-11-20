e1ef43a2cbd3920ee31e30638e51d06285e03b01
Mark Evenson <evenson.not.org@gmail.com>
2019-11-20 16:39:32 +0100

    ci: fix reference to buildapp source location

--- ci/install-buildapp.bash
+++ ci/install-buildapp.bash
@@ -3,10 +3,12 @@
 dist_name=buildapp
 dist_file=${dist_name}.tgz
 dist_uri="http://www.xach.com/lisp/${dist_file}"
+# FIXME derive from sed-ing the tar contents?
+dist_version="1.5.6"
 
 wget ${dist_uri}
 tar xfvz ${dist_file}
-pushd ${dist_name} && ./configure && make && sudo make install
+pushd ${dist_name}-${dist_version} && ./configure && make && sudo make install
 popd
 
 
