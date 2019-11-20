4e1991177e5ce1a8e74e36cce46806ebd5b0e621
Mark Evenson <evenson.not.org@gmail.com>
2019-11-20 12:39:20 +0100

    ci: install dependencies not available from Quicklisp

---
+++ ci/non-quicklisp-dependencies.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env
+
+local_projects=$HOME/quicklisp/local-projects
+
+mkdir -p ${local_projects}
+
+repos=""
+repos += "https://github.com/norvig/paip-lisp "
+repos += "https://github.com/guitarvydas/loops "
+
+
+cd ${local_projects}
+for repo in repos; do
+    git clone ${repo}
+done
+    
