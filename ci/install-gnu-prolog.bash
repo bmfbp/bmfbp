3b136f8034908d75738a6d86f36ab35034a1c5b2
Mark Evenson <evenson.not.org@gmail.com>
2019-11-20 16:24:32 +0100

    ci:  need sudo to install to system locations

--- ci/install-gnu-prolog.bash
+++ ci/install-gnu-prolog.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 dist_name=gprolog-1.4.5
 dist_file=${dist_name}.tar.gz
 dist_uri=http://gprolog.org/${dist_file}
@@ -9,7 +8,8 @@ wget ${dist_uri}
 
 tar xfvz ${dist_file}
 pushd ${dist_name}
-pushd src && ./configure && make && make install
+# TODO mark or refactor upwards the use of sudo
+pushd src && ./configure && make && sudo make install
 popd
 popd
 
