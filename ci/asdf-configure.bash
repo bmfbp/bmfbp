9c5ecdf7f5b76545cb6889935c036a43cf311882
Mark Evenson <evenson.not.org@gmail.com>
2019-11-20 12:16:14 +0100

    ci: point the ASDF configuration TRAVIS_BUILD_DIR

--- ci/asdf-configure.bash
+++ ci/asdf-configure.bash
@@ -6,6 +6,9 @@ dest=$HOME/.config/common-lisp/source-registry.conf.d
 
 mkdir -p $HOME
 
+# Locate the root of the source repository
+bmfbp=${TRAVIS_BUILD_DIR}
+
 # TODO figure out how to sanely address paths
-echo '(:tree (:home "build/easye/bmfbp/"))' > ${dest}/bmfpb.conf
+echo '(:tree "${bmfbp}")' > ${dest}/bmfpb.conf
 
