430c8c059093cc77a7104e7bb5bae306ad7530f7
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-29 13:21:00 -0500

    @inject is deprecated, replaced by @send

--- clparts/test.lisp
+++ clparts/test.lisp
@@ -1,13 +1,14 @@
-(in-package :arrowgrams)
+(in-package :arrowgrams/clparts)
 
 (defun test ()
   (cl-event-passing-user::@enable-logging)
   (let ((net (parser-builder)))
     (let ((ap e/dispatch::*all-parts*)) ;; testing only
       (assert (= 4 (length ap)))        ;; testing only
-      (cl-event-passing-user::@inject
+      (cl-event-passing-user::@send
        net
        (e/part::get-input-pin net :peg-source-file-name)
        (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg"))
-      (cl-event-passing-user::@history))))
+      (cl-event-passing-user::@history))
+    (setf arrowgrams/clparts::*parser-builder-net* net)))
 
