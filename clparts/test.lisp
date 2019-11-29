ce4dbaa080940b0e82c39f8f3a609a99299d1cc5
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-29 13:42:26 -0500

    added test-macro and test-all

--- clparts/test.lisp
+++ clparts/test.lisp
@@ -12,3 +12,25 @@
       (cl-event-passing-user::@history))
     (setf arrowgrams/clparts::*parser-builder-net* net)))
 
+(defun test-macro ()
+  (cl-event-passing-user::@enable-logging)
+  (let ((net
+         (cl-event-passing-user::@defnetwork 
+          main ;; TODO: is this ALWAYS the same as the last declaration?
+          (:part (parser-builder) pbuilder (:peg-source-file-name) (:lisp-source-out :fatal-error))
+          (:schem main (:peg-source-file-name) (:lisp-source-out)
+           (pbuilder) ;; internal parts
+           ;; wiring
+           ((((:self :peg-source-file-name)) ((pbuilder :peg-source-file-name)))
+            (((pbuilder :lisp-souce-out)) ((:self :lisp-source-out)))))))
+        (peg-filename (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg")))
+    (cl-event-passing-user::@send net (e/part::get-input-pin net :peg-source-file-name) peg-filename)))
+
+(defun arrowgrams/clparts::test-all ()
+  (format *standard-output* "~&running test~%")
+  (test)
+  (format *standard-output* "~&running test-macro~%")
+  (test-macro))
+
+      
+     
