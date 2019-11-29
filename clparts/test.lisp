dd65b27e942e05c919bb19fafa0401e1560ca4a0
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-29 16:05:50 -0500

    remove possible name conflict of "main"

--- clparts/test.lisp
+++ clparts/test.lisp
@@ -16,21 +16,23 @@
   (cl-event-passing-user::@enable-logging)
   (let ((net
          (cl-event-passing-user::@defnetwork 
-          main ;; TODO: is this ALWAYS the same as the last declaration?
+          XXX ;; TODO: is this ALWAYS the same as the last declaration?
           (:part (parser-builder) pbuilder (:peg-source-file-name) (:lisp-source-out :fatal-error))
-          (:schem main (:peg-source-file-name) (:lisp-source-out)
+          (:schem XXX (:peg-source-file-name) (:lisp-source-out)
            (pbuilder) ;; internal parts
            ;; wiring
            ((((:self :peg-source-file-name)) ((pbuilder :peg-source-file-name)))
-            (((pbuilder :lisp-souce-out)) ((:self :lisp-source-out)))))))
-        (peg-filename (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg")))
-    (cl-event-passing-user::@send net (e/part::get-input-pin net :peg-source-file-name) peg-filename)))
+            (((pbuilder :lisp-souce-out)) ((:self :lisp-source-out))))))))
+    (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg"))
+          (in-pin (e/part::get-input-pin net :peg-source-file-name)))
+      (cl-event-passing-user::@send net in-pin peg-filename))))
 
 (defun arrowgrams/clparts::test-all ()
   (format *standard-output* "~&running test~%")
   (test)
   (format *standard-output* "~&running test-macro~%")
-  (test-macro))
+  (test-macro)
+  (cl-event-passing-user::@history))
 
       
      
