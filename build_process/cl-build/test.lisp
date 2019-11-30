fba336363e81a1f2752f01d60bcce49c3e5f5dee
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-30 12:19:44 -0500

    no whitespace version

--- build_process/cl-build/test.lisp
+++ build_process/cl-build/test.lisp
@@ -1,8 +1,12 @@
 (in-package :arrowgrams/build/cl-build)
 
 (defun arrowgrams/build/cl-build::test-all ()
-  (format *standard-output* "~&running json parer~%")
-  (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "test.peg"))
-        (json-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "test.json")))
+  (format *standard-output* "~&running json parser~%")
+  (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/test.peg"))
+        (json-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/test-nowhitespace.json")))
     (json-parser peg-filename json-filename)))
 
+#|
+(ql:quickload :arrowgrams/build/cl-build/test)
+(arrowgrams/build/cl-build::test-all)
+|#
