507c48eb3008cf4bb0a8bebf4dd22d4f243d525b
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-30 08:31:18 -0500

    successful reuse of parser-builder

--- clparts/test.lisp
+++ clparts/test.lisp
@@ -22,7 +22,7 @@
            (pbuilder) ;; internal parts
            ;; wiring
            ((((:self :peg-source-file-name)) ((pbuilder :peg-source-file-name)))
-            (((pbuilder :lisp-souce-out)) ((:self :lisp-source-out))))))))
+            (((pbuilder :lisp-source-out)) ((:self :lisp-source-out))))))))
     (e/dispatch::ensure-correct-number-of-parts (+ 1 4))
     (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg"))
           (in-pin (e/part::get-input-pin net :peg-source-file-name)))
@@ -30,27 +30,30 @@
 
 (defun test-macro-with-call ()
   ;; the only difference from above it "(parser-builder)" instead of "*parser-builder-net*"
-  (cl-event-passing-user::@enable-logging)
-  (let ((net
-         (cl-event-passing-user::@defnetwork 
-          XXX ;; TODO: is this ALWAYS the same as the last declaration?
-          (:part (parser-builder) pbuilder (:peg-source-file-name) (:lisp-source-out :fatal-error))
-          (:schem XXX (:peg-source-file-name) (:lisp-source-out)
-           (pbuilder) ;; internal parts
-           ;; wiring
-           ((((:self :peg-source-file-name)) ((pbuilder :peg-source-file-name)))
-            (((pbuilder :lisp-souce-out)) ((:self :lisp-source-out))))))))
-    (e/dispatch::ensure-correct-number-of-parts (+ 1 4))
-    (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg"))
-          (in-pin (e/part::get-input-pin net :peg-source-file-name)))
-      (cl-event-passing-user::@send net in-pin peg-filename))))
-
+  (let ((parser-builder-net (parser-builder))) ;; this call add parts to the dispatcher list
+    (cl-event-passing-user::@initialize)       ;; this clears the dispatcher list
+    (cl-event-passing-user::@enable-logging)
+    (let ((net
+           (cl-event-passing-user::@defnetwork 
+            XXX ;; TODO: is this ALWAYS the same as the last declaration?
+            (:part parser-builder-net pbuilder (:peg-source-file-name) (:lisp-source-out :fatal-error))
+            (:schem XXX (:peg-source-file-name) (:lisp-source-out)
+             (pbuilder) ;; internal parts
+             ;; wiring
+             ((((:self :peg-source-file-name)) ((pbuilder :peg-source-file-name)))
+              (((pbuilder :lisp-source-out)) ((:self :lisp-source-out))))))))
+      (e/dispatch::ensure-correct-number-of-parts (+ 1 4))
+      (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/clparts "clparts/test.peg"))
+            (in-pin (e/part::get-input-pin net :peg-source-file-name)))
+        (cl-event-passing-user::@send net in-pin peg-filename)))))
+  
 (defun arrowgrams/clparts::test-all ()
   (format *standard-output* "~&running test~%")
   (test)
   (format *standard-output* "~&running test-macro~%")
   (test-macro-with-global)
-  #+nil(cl-event-passing-user::@history))
+  (format *standard-output* "~&running test-macro with direct call~%")
+  (test-macro-with-call))
 
       
      
