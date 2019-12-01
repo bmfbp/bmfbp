0526a13ea1b597a607a57228fa36a72c7019c0b2
Paul Tarvydas <guitarvydas@gmail.com>
2019-12-01 08:47:00 -0500

    wip: text t in json.peg gets rid of most nils

--- build_process/cl-build/test.lisp
+++ build_process/cl-build/test.lisp
@@ -3,9 +3,10 @@
 (defun arrowgrams/build/cl-build::test-all ()
   (format *standard-output* "~&running json parser~%")
   (let ((debug-rules '(json value j-object members member j-array elements element j-string)))
-    (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/json-parse-only.peg"))
+    (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/json.peg"))
           (json-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/test.json")))
-      (json-parser peg-filename json-filename :debug debug-rules))))
+      ;(json-parser peg-filename json-filename :debug debug-rules))))
+      (json-parser peg-filename json-filename))))
 
 #|
 first time:
