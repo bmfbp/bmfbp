f5ba4542be4e50edb2d8d37bc5c00d806c10bac8
Paul Tarvydas <guitarvydas@gmail.com>
2019-12-01 09:36:47 -0500

    wip: fixed strip-quotes

--- build_process/cl-build/util.lisp
+++ build_process/cl-build/util.lisp
@@ -1,4 +1,10 @@
 (in-package :arrowgrams/build/cl-build)
 
 (defun strip-quotes (s)
-  (esrap:text s))
+  (let ((s (esrap:text s)))
+    (if (and
+         (char= (char s 0) #\")
+         (char= (char s (1- (length s))) #\"))
+        (subseq s 1 (1- (length s)))
+      s)))
+         
