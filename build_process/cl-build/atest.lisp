5787a12e575852a4faeb6d4f6a90b86a49128f8b
Paul Tarvydas <guitarvydas@gmail.com>
2019-12-04 05:31:16 -0500

    extend trace list

--- build_process/cl-build/atest.lisp
+++ build_process/cl-build/atest.lisp
@@ -2,7 +2,7 @@
 
 (defun arrowgrams/build/cl-build::atest ()
   (format *standard-output* "~&running pseudo code parser~%")
-  (let ((debug-rules '(pseudo-grammar)))
+  (let ((debug-rules '(pseudo-grammar top-level-description part-declarations internal-parts wiring)))
     (let ((peg-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/pseudo.peg"))
           (pseudo-filename (asdf:system-relative-pathname :arrowgrams/build/cl-build "build_process/cl-build/build-process.pseudo"))
           (grammar-name 'arrowgrams/build/cl-build::pseudo-grammar))
