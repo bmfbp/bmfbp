1d326206afe90eca59976f7738bbf7a20fde5eff
Paul Tarvydas <guitarvydas@gmail.com>
2019-12-18 10:29:43 -0500

    added macros

---
+++ svg/cl-compiler/classes.lisp
@@ -0,0 +1,18 @@
+(in-package :arrowgrams/compiler/classes)
+
+(defmacro with-speechbubble-bb (id-list bb &body body)
+  `(let ((left (cdr (second ,bb)))
+         (top  (cdr (third ,bb)))
+         (right  (cdr (fourth ,bb)))
+         (bottom  (cdr (fifth ,bb))))
+     ,@body))
+
+(defmacro with-text-bb (id-list bb &body body)
+  `(let ((id (cdr (first ,bb)))
+         (left (cdr (third ,bb)))
+         (top  (cdr (fourth ,bb)))
+         (right  (cdr (fifth ,bb)))
+         (bottom  (cdr (sixth ,bb))))
+     ,@body))
+
+
