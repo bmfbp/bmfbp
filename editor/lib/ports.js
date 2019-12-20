405413d623308d86456bc6b8580605ff61ef2806
Ken Kan <kenhkan@gmail.com>
2019-12-19 21:13:40 -0500

    Output JSON

--- editor/lib/ports.js
+++ editor/lib/ports.js
@@ -1,6 +1,6 @@
 app.ports.saveFile.subscribe(function(data) {
   const filetype = 'text/plain;charset=utf-8';
-  const filename = 'diagram.lisp';
+  const filename = 'diagram.json';
   const output = JSON.stringify(data);
   saveAs(new Blob([output], { type: filetype }), filename);
 });
