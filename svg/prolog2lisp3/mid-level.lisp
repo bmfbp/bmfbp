289da751160a0053c93810bb75560dc9f60fe98e
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-01 12:13:28 -0500

    match WS* in mid-level, use compose to filter results

--- svg/prolog2lisp3/mid-level.lisp
+++ svg/prolog2lisp3/mid-level.lisp
@@ -1,13 +1,19 @@
 (in-package :arrowgrams/parser)
 
-(defrule tInt (+ (character-ranges (#\0 #\9)))
-  (:text t) (:function parse-integer) (:lambda (x) `(int ,x)))
+(defrule tInt (and (+ (character-ranges (#\0 #\9)))
+                   (* tWS))
+  (:destructure (x spc) (declare (ignore spc)) x)                 
+  (:text t)
+  (:function parse-integer)
+  (:lambda (x) `(int ,x)))
 
 (defrule tVar (and (or tDontCare tNamedVar) (* tWS))
   (:destructure (x spc) (declare (ignore spc)) x)
   (:lambda (x) `(var ,x))) 
 
-(defrule tIdent (and tLowerCaseLetter (* tOtherLetter))
-  (:text t) (:lambda (x) `(ident ,x)))
+(defrule tIdent (and tLowerCaseLetter (* tOtherLetter) (* tWS))
+  (:destructure (x spc) (declare (ignore spc)) x)                 
+  (:text t)
+  (:lambda (x) `(ident ,x)))
 
 
