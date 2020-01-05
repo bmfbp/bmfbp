1950a7f3176bffe3f3e46e498ec128cd1f50f60f
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-05 17:17:39 -0500

    testing: add-kinds

--- svg/cl-compiler/add-kinds.lisp
+++ svg/cl-compiler/add-kinds.lisp
@@ -85,7 +85,8 @@
 (defmethod add-kinds ((self e/part:part))
   (let ((fb
          (append
-          (append arrowgrams/compiler::+manually-defined-rules+ arrowgrams/compiler::+rules+)
+          arrowgrams/compiler::+rules+
           (cl-event-passing-user::@get-instance-var self :fb)))
-        (goal '((:trace-on 2) (:add_kinds_main))))
-          (arrowgrams/compiler/util::run-prolog self goal fb)))
+       ;(goal '((:trace-on 1) (:add_kinds_main))))
+        (goal '((:add_kinds_main))))
+    (arrowgrams/compiler/util::run-prolog self goal fb)))
