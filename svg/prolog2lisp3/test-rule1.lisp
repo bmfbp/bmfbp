16b7d5d9c10b18228a4b2e9f36efa4a32b28e74f
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-06 21:01:01 -0500

    very small test being invoked by (CTEST) (arrowgrams/parser::create), (arrowgrams/compiler::compiler)

--- svg/prolog2lisp3/test-rule1.lisp
+++ svg/prolog2lisp3/test-rule1.lisp
@@ -2,15 +2,14 @@
 
 (defparameter *test1*
 "
-rect(id1).
-
-findrects(ID) :- rect(ID),fail.
+findrects(ID) :- rect(ID),we('rect '),wen(ID),fail.
 findrects(_).
 
-printAll :- findrects(ID),we('rect '),wen(ID).
+printAll :- findrects(ID),we('printall '),wen(ID).
 
 we(WE_ARG) :- write(user_output,WE_ARG).
 wen(WEN_arg):- we(WEN_arg),nle.
+nle :- nl(user_output).
 ")
 
 
