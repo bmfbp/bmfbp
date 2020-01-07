64ab9c9a549f581f89e4814565838d7b8e58af11
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-07 08:28:15 -0500

    fixed comparison script

--- svg/cl-compiler/comp.bash
+++ svg/cl-compiler/comp.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
 sort output.prolog | ./stripfloats.bash >output2.prolog
-sort ../js-compiler/$(1) | ./stripfloats.bash >temp.pro
+sort ../js-compiler/$1 >xxx
+./stripfloats.bash <xxx >temp.pro
 diff -i output2.prolog temp.pro
