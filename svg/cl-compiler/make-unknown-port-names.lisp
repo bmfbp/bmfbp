45bdab4b415e22360655964762693b732ac0911b
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-10 09:55:59 -0500

    enabled make-unknown-port-names pass

--- svg/cl-compiler/make-unknown-port-names.lisp
+++ svg/cl-compiler/make-unknown-port-names.lisp
@@ -28,7 +28,7 @@
            (progn
              (cl-event-passing-user::@set-instance-var self :fb data)
              (format *standard-output* "~&make-unknown-port-names~%")
-             ;; put code here
+             (make-unknown-port-names self)
              (cl-event-passing-user::@send self :done t)
              (cl-event-passing-user::@set-instance-var self :state :idle))
          (cl-event-passing-user::@send
@@ -36,3 +36,10 @@
           :error
           (format nil "MAKE-UNKNOWN-PORT-NAMES in state :waiting-for-new-fb expected :fb, but got action ~S data ~S" pin (e/event:data e))))))))
 
+(defmethod make-unknown-port-names ((self e/part:part))
+  (let ((fb
+         (append
+          arrowgrams/compiler::*rules*
+          (cl-event-passing-user::@get-instance-var self :fb)))
+        (goal '((:make_unknown_port_names_main))))
+    (arrowgrams/compiler/util::run-prolog self goal fb)))
