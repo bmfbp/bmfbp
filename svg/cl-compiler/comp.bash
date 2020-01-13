c1a3b63d26787ab2b49d11087ff429de7c24c080
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-13 16:46:51 -0500

    adjust bash files to strip wirenum before diff (numbers depend on search sequence)

--- svg/cl-compiler/comp.bash
+++ svg/cl-compiler/comp.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
-sort output.prolog | ./stripfloats.bash >output2.prolog
+sort output.prolog | ./stripfloats.bash | ./strip-wirenum.bash >output2.prolog
 sort ../js-compiler/$1 >xxx
-./stripfloats.bash <xxx >temp.pro
+./stripfloats.bash <xxx | ./strip-wirenum.bash >temp.pro
 rm -f xxx
 diff -iu output2.prolog temp.pro
