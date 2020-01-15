3939db3669eaf6aa2eff6f45ef834b4dce8eb188
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-15 07:47:18 -0500

    attempt at untracing specific low-level rules

--- svg/xform/xform.lisp
+++ svg/xform/xform.lisp
@@ -6,7 +6,7 @@
   (let ((example-as-string 
 	 (alexandria:read-file-into-string
           (asdf:system-relative-pathname :arrowgrams "svg/xform/test.ir"))))
-
+#|
     (esrap:trace-rule 'arrowgrams-intermediate-representation)
     (esrap:trace-rule '<self-Part>)
     (esrap:trace-rule '<self-kind>)
@@ -27,6 +27,21 @@
     (esrap:trace-rule '<part-id-or-self>)
     (esrap:trace-rule '<part-id>)
     (esrap:trace-rule '<pin-id>)
+
+    (esrap:trace-rule '<react-function>)
+    (esrap:trace-rule '<first-time-function>)
   
+|#
+    (esrap:trace-rule 'arrowgrams-intermediate-representation :recursive t)
+    (esrap:untrace-rule '<not-dquote>)
+    (esrap:untrace-rule 'LPAR)
+    (esrap:untrace-rule 'RPAR)
+    (esrap:untrace-rule 'WS)
+    (esrap:untrace-rule '<white-space>)
+    (esrap:untrace-rule '<comment>)
+    (esrap:untrace-rule '<end-of-input>)
+    (esrap:untrace-rule '<end-of-line>)
+    (esrap:untrace-rule '<same-line>)
+
     (esrap:parse 'arrowgrams-intermediate-representation example-as-string)))
 
