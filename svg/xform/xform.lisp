ec06b2d4cd69e0c92fcc7687798a3fba877cdb73
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-15 08:00:35 -0500

    untracing specific rules

--- svg/xform/xform.lisp
+++ svg/xform/xform.lisp
@@ -33,15 +33,18 @@
   
 |#
     (esrap:trace-rule 'arrowgrams-intermediate-representation :recursive t)
+
     (esrap:untrace-rule '<not-dquote>)
-    (esrap:untrace-rule 'LPAR)
-    (esrap:untrace-rule 'RPAR)
+    ;(esrap:untrace-rule 'LPAR)
+    ;(esrap:untrace-rule 'RPAR)
     (esrap:untrace-rule 'WS)
     (esrap:untrace-rule '<white-space>)
     (esrap:untrace-rule '<comment>)
     (esrap:untrace-rule '<end-of-input>)
     (esrap:untrace-rule '<end-of-line>)
     (esrap:untrace-rule '<same-line>)
+    (esrap:untrace-rule 'STRING)
+    (esrap:untrace-rule 'IDENT)
 
     (esrap:parse 'arrowgrams-intermediate-representation example-as-string)))
 
