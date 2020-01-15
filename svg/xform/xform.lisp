843a154913da46a4633195f267786f87f4353c17
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-15 08:02:05 -0500

    success - turn off tracing

--- svg/xform/xform.lisp
+++ svg/xform/xform.lisp
@@ -6,32 +6,8 @@
   (let ((example-as-string 
 	 (alexandria:read-file-into-string
           (asdf:system-relative-pathname :arrowgrams "svg/xform/test.ir"))))
-#|
-    (esrap:trace-rule 'arrowgrams-intermediate-representation)
-    (esrap:trace-rule '<self-Part>)
-    (esrap:trace-rule '<self-kind>)
-    (esrap:trace-rule '<self-inputs>)
-    (esrap:trace-rule '<self-outputs>)
-    (esrap:trace-rule '<self-wiring>)
-    (esrap:trace-rule '<self-part-decls>)
-    (esrap:trace-rule '<wire>)
-    (esrap:trace-rule '<wire-id>)
-    (esrap:trace-rule '<froms>)
-    (esrap:trace-rule '<tos>)
-    (esrap:trace-rule '<part-pin>)
-    (esrap:trace-rule '<part-decl>)
-    (esrap:trace-rule '<id>)
-    (esrap:trace-rule '<kind>)
-    (esrap:trace-rule '<inputs>)
-    (esrap:trace-rule '<outputs>)
-    (esrap:trace-rule '<part-id-or-self>)
-    (esrap:trace-rule '<part-id>)
-    (esrap:trace-rule '<pin-id>)
 
-    (esrap:trace-rule '<react-function>)
-    (esrap:trace-rule '<first-time-function>)
-  
-|#
+#|
     (esrap:trace-rule 'arrowgrams-intermediate-representation :recursive t)
 
     (esrap:untrace-rule '<not-dquote>)
@@ -45,6 +21,7 @@
     (esrap:untrace-rule '<same-line>)
     (esrap:untrace-rule 'STRING)
     (esrap:untrace-rule 'IDENT)
+|#
 
     (esrap:parse 'arrowgrams-intermediate-representation example-as-string)))
 
