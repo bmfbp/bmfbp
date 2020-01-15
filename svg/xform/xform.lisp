16e4ae4f28f542134b8d9aa97bc4cc4203132474
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-14 22:57:42 -0500

    more detailed tracing (eschew :recursive)

--- svg/xform/xform.lisp
+++ svg/xform/xform.lisp
@@ -6,6 +6,27 @@
   (let ((example-as-string 
 	 (alexandria:read-file-into-string
           (asdf:system-relative-pathname :arrowgrams "svg/xform/test.ir"))))
-    (esrap:trace-rule 'arrowgrams-intermediate-representation :recursive t)
+
+    (esrap:trace-rule 'arrowgrams-intermediate-representation)
+    (esrap:trace-rule '<self-Part>)
+    (esrap:trace-rule '<self-kind>)
+    (esrap:trace-rule '<self-inputs>)
+    (esrap:trace-rule '<self-outputs>)
+    (esrap:trace-rule '<self-wiring>)
+    (esrap:trace-rule '<self-part-decls>)
+    (esrap:trace-rule '<wire>)
+    (esrap:trace-rule '<wire-id>)
+    (esrap:trace-rule '<froms>)
+    (esrap:trace-rule '<tos>)
+    (esrap:trace-rule '<part-pin>)
+    (esrap:trace-rule '<part-decl>)
+    (esrap:trace-rule '<id>)
+    (esrap:trace-rule '<kind>)
+    (esrap:trace-rule '<inputs>)
+    (esrap:trace-rule '<outputs>)
+    (esrap:trace-rule '<part-id-or-self>)
+    (esrap:trace-rule '<part-id>)
+    (esrap:trace-rule '<pin-id>)
+  
     (esrap:parse 'arrowgrams-intermediate-representation example-as-string)))
 
