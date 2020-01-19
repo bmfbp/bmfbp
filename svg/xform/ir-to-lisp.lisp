0f224e30895f1bbdb1fe8b15baa4a02db4d4e993
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-19 12:05:45 -0500

    converted <x...> rules to {...}

--- svg/xform/ir-to-lisp.lisp
+++ svg/xform/ir-to-lisp.lisp
@@ -1,57 +1,57 @@
 (in-package :arrowgrams/compiler/xform)
 
 (esrap:defrule ir-to-lisp-grammar
-    (and (* WS) LPAR <xself-Part> RPAR <end-of-input>)
+    (and (* WS) LPAR {self-Part} RPAR <end-of-input>)
   (:function third))
 
-(esrap:defrule <xself-Part> 
-    (and <xself-kind> <xself-inputs> <xself-outputs> <xreact-function> <xfirst-time-function> <xself-part-decls> <xself-wiring>)
+(esrap:defrule {self-Part}
+    (and {self-kind} {self-inputs} {self-outputs} {react-function} {first-time-function} {self-part-decls} {self-wiring})
   (:destructure (kind inputs outputs react first-time parts wiring)
 		(declare (ignore kind react first-time))
 		(list inputs outputs parts wiring)))
 
 
-(esrap:defrule <xself-kind> IDENT)
+(esrap:defrule {self-kind} IDENT)
 
-(esrap:defrule <xself-inputs> <xinputs>)
+(esrap:defrule {self-inputs} {inputs})
 
-(esrap:defrule <xself-outputs> <xoutputs>)
+(esrap:defrule {self-outputs} {outputs})
 
-(esrap:defrule <xself-wiring> (and LPAR (* <xwire>) RPAR)
+(esrap:defrule {self-wiring} (and LPAR (* {wire}) RPAR)
   (:function second))
 
-(esrap:defrule <xself-part-decls> (and LPAR (* <xpart-decl>) RPAR))
+(esrap:defrule {self-part-decls} (and LPAR (* {part-decl}) RPAR))
 
 
-(esrap:defrule <xwire> (and LPAR <xwire-id> <xfroms> <xtos> RPAR)
+(esrap:defrule {wire} (and LPAR {wire-id} {froms} {tos} RPAR)
   (:destructure (lp wire-id froms tos rp)
 		(declare (ignore lp rp))
 		(list wire-id froms tos)))
 
-(esrap:defrule <xwire-id> (or IDENT <INTEGER>))
-(esrap:defrule <xfroms> (and LPAR (* <xpart-pin>) RPAR))
-(esrap:defrule <xtos> (and LPAR (* <xpart-pin>) RPAR))
+(esrap:defrule {wire-id} (or IDENT <INTEGER>))
+(esrap:defrule {froms} (and LPAR (* {part-pin}) RPAR))
+(esrap:defrule {tos} (and LPAR (* {part-pin}) RPAR))
 
-(esrap:defrule <xpart-pin> (and LPAR <xpart-id-or-self> <xpin-id> RPAR))
+(esrap:defrule {part-pin} (and LPAR {part-id-or-self} {pin-id} RPAR))
 
-(esrap:defrule <xpart-decl> (and LPAR <xid> <xkind> <xinputs> <xoutputs> <xreact-function> <xfirst-time-function> RPAR))
+(esrap:defrule {part-decl} (and LPAR {id} {kind} {inputs} {outputs} {react-function} {first-time-function} RPAR))
 
-(esrap:defrule <xid> IDENT)
+(esrap:defrule {id} IDENT)
 
-(esrap:defrule <xkind> IDENT)
+(esrap:defrule {kind} IDENT)
 
-(esrap:defrule <xinputs> (or <nil> <xpin-list>))
+(esrap:defrule {inputs} (or <nil> {pin-list}))
 
-(esrap:defrule <xpin-list> (and LPAR (* <xpin-id>) RPAR)
+(esrap:defrule {pin-list} (and LPAR (* {pin-id}) RPAR)
   (:function second))
 
-(esrap:defrule <xoutputs> (and LPAR (* <xpin-id>) RPAR)
+(esrap:defrule {outputs} (and LPAR (* {pin-id}) RPAR)
   (:function second))
 
 
-(esrap:defrule <xpart-id-or-self> (or <xpart-id> <xself-keyword>))
+(esrap:defrule {part-id-or-self} (or {part-id} {self-keyword}))
 
-(esrap:defrule <xreact-function> IDENT)
-(esrap:defrule <xfirst-time-function> IDENT)
-(esrap:defrule <xpart-id> IDENT)
-(esrap:defrule <xpin-id> IDENT)
+(esrap:defrule {react-function} IDENT)
+(esrap:defrule {first-time-function} IDENT)
+(esrap:defrule {part-id} IDENT)
+(esrap:defrule {pin-id} IDENT)
