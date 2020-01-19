4f0b64e4ba017e3800d1cb6472c63bbacc357e71
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-19 09:02:19 -0500

    tracing keyword and xtest functions

--- svg/xform/xform.lisp
+++ svg/xform/xform.lisp
@@ -2,27 +2,26 @@
 
 (cl-peg:into-package :arrowgrams/compiler/xform)
 
-(defun parse-ir ()
+(defun parse-ir (filename :key (trace nil))
   (let ((example-as-string 
 	 (alexandria:read-file-into-string
-          ;(asdf:system-relative-pathname :arrowgrams "svg/xform/test.ir"))))
-          (asdf:system-relative-pathname :arrowgrams "svg/xform/test2.lisp"))))
-
-#|
-    (esrap:trace-rule 'arrowgrams-intermediate-representation :recursive t)
-
-    (esrap:untrace-rule '<not-dquote>)
-    ;(esrap:untrace-rule 'LPAR)
-    ;(esrap:untrace-rule 'RPAR)
-    (esrap:untrace-rule 'WS)
-    (esrap:untrace-rule '<white-space>)
-    (esrap:untrace-rule '<comment>)
-    (esrap:untrace-rule '<end-of-input>)
-    (esrap:untrace-rule '<end-of-line>)
-    (esrap:untrace-rule '<same-line>)
-    (esrap:untrace-rule 'STRING)
-    (esrap:untrace-rule 'IDENT)
-|#
+          (asdf:system-relative-pathname :arrowgrams filename))))
+    (when trace
+      (esrap:trace-rule 'arrowgrams-intermediate-representation :recursive t)
+      (esrap:untrace-rule '<not-dquote>)
+      (esrap:untrace-rule 'WS)
+      (esrap:untrace-rule '<white-space>)
+      (esrap:untrace-rule '<comment>)
+      (esrap:untrace-rule '<end-of-input>)
+      (esrap:untrace-rule '<end-of-line>)
+      (esrap:untrace-rule '<same-line>)
+      (esrap:untrace-rule 'STRING)
+      (esrap:untrace-rule 'IDENT))
 
     (esrap:parse 'arrowgrams-intermediate-representation example-as-string)))
 
+(defun xtest ()
+  (parse-ir (asdf:system-relative-pathname :arrowgrams "svg/cl-compiler/BUILD_PROCESS.ir")))
+
+(defun cl-user::xtest ()
+  (arrowgrams/compiler/xform::xtest :trace t))
