793837f61c0483500f60192a6540a314551f7cb9
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-20 10:58:37 -0500

    rewritten by cloning symbols

--- svg/back-end/strings.lisp
+++ svg/back-end/strings.lisp
@@ -3,72 +3,73 @@
 ; (:code strings (:token) (:request :out :error) #'strings-react #'strings-first-time)
 
 (defparameter *strings-buffer* nil)
-(defparameter *strings-position* 0)
+(defparameter *strings-start-position* 0)
 (defparameter *strings-state* :idle)
 
-(defun strings-clear-buffer (pos)
-  (setf *strings-buffer* nil)
-  (setf *strings-position* pos))
-
-(defun strings-push-char-into-buffer (c)
-  (push c *strings-buffer*))
-
 (defun strings-get-buffer () (coerce (reverse *strings-buffer*) 'string))
-(defun strings-get-position () *strings-position*)
+(defun strings-get-position () *strings-start-position*)
 
 (defmethod strings-first-time ((self e/part:part))
   (setf *strings-state* :idle)
   )
 
 (defmethod strings-react ((self e/part:part) (e e/event:event))
-  (labels ((new-string () (make-token :kind :string :text (strings-get-buffer) :position (strings-get-position)))
-           (check-eof ()
-             (when (eq :eof (token-kind (e/event:data e)))
-               (setf *strings-state* :done)))
-           (pull () (send! self :request t))
-           (forward-token () (send-event! self :out e)))
-    ;(format *standard-output* "~&strings ~S ~S~%" *strings-state* (e/event:data e))
+  (labels ((push-char-into-buffer () (push (token-text (e/event:data e)) *strings-buffer*))
+           (pull () (send! self :request t) (format *standard-output* "~&strings pull~%"))
+           (forward-token () (send-event! self :out e) (format *standard-output* "~&strings forwards token ~S~%" e))
+           (start-char-p () 
+             (when (eq :character (token-kind (e/event:data e)))
+               (let ((c (token-text (e/event:data e))))
+                 (char= c #\"))))
+           (follow-char-p ()
+             (when (eq :character (token-kind (e/event:data e)))
+               (let ((c (token-text (e/event:data e))))
+                 (not (char= c #\")))))
+           (action () (e/event::sym e))
+           (next-state (x) (setf *strings-state* x))
+           (eof-p () (eq :eof (token-kind (e/event:data e))))
+           (clear-buffer ()
+             (setf *strings-buffer* nil)
+             (setf *strings-start-position* (token-position (e/event:data e))))
+           (release-buffer ()
+             (format *standard-output* "~&strings release-buffer~%")
+             (send! self :out (make-token :kind :symbol :text (strings-get-buffer) :position (strings-get-position))))
+           (release-and-clear-buffer ()
+             (release-buffer)
+             (clear-buffer))
+         )
+
+    (format *standard-output* "~&strings in state ~S gets ~S ~S~%" *strings-state* (token-kind (e/event:data e)) (token-text (e/event:data e)))
     (ecase *strings-state*
       (:idle
-       (ecase (e/event::sym e)
+       (ecase (action)
          (:token
-          (let ((tok (e/event:data e)))
-            (cond ((eq :character (token-kind tok))
-                   (let ((c (token-text tok)))
-                     (case c
-                       (#\"
-                        (strings-clear-buffer (token-position tok))
-                        (strings-push-char-into-buffer c)
-                        (pull)
-                        (setf *strings-state* :collecting-string))
-                       (otherwise (forward-token)
-                                  (check-eof)))))
-                  (t
-                   (forward-token)
-                   (check-eof)))))))
-      
-      (:collecting-string
-       (let ((tok (e/event:data e)))
-         (cond ((eq :character (token-kind tok))
-                (let ((c (token-text tok)))
-                  (case c
-                    (#\"
-                     (strings-push-char-into-buffer c)
-                     (let ((str-token (new-string)))
-                       (strings-clear-buffer (token-position tok))
-                       (send! self :out str-token)
-                       (setf *strings-state* :idle)))
-                    (otherwise
-                     (strings-push-char-into-buffer c)
-                     (pull)))))
-               ((eq :EOF (token-kind tok))
-                (send! self :error (format nil "incomplete string at ~A" (strings-get-position))))
-               (t (assert nil)))))
-      
+          (if (eof-p)
+              (progn
+                (forward-token)
+                (next-state :done))
+            (if (start-char-p)
+                (progn
+                  (push-char-into-buffer)
+                  (pull)
+                  (next-state :collecting-symbol))
+              (forward-token))))))
+      (:collecting-symbol
+       (ecase (action)
+         (:token
+          (if (eof-p)
+              (progn
+                (release-and-clear-buffer)
+                (forward-token)
+                (next-state :done))
+            (if (follow-char-p)
+                (progn
+                  (push-char-into-buffer)
+                  (pull))
+              (progn
+                (release-and-clear-buffer)
+                (forward-token)
+                (next-state :idle)))))))
       (:done
-       (send! self :error (format nil "strings done, but received token ~S" (e/event:data e)))))))
+       (send! self :error (format nil "strings finished, but received ~S" e))))))
 
-        
-     
-
-     
