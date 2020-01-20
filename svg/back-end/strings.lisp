5a3b1c088a29c0613d8139190a7338349790eccf
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-19 23:22:46 -0500

    wip: break before cutting symbols in

--- svg/back-end/strings.lisp
+++ svg/back-end/strings.lisp
@@ -6,22 +6,22 @@
 (defparameter *strings-position* 0)
 (defparameter *strings-state* :idle)
 
-(defun clear-buffer (pos)
+(defun symbols-clear-buffer (pos)
   (setf *strings-buffer* nil)
   (setf *strings-position* pos))
 
-(defun push-char-into-buffer (c)
+(defun symbols-push-char-into-buffer (c)
   (push c *strings-buffer*))
 
-(defun get-buffer () (coerce (reverse *strings-buffer*) 'string))
-(defun get-position () *strings-position*)
+(defun symbols-get-buffer () (coerce (reverse *strings-buffer*) 'string))
+(defun symbols-get-position () *strings-position*)
 
 (defmethod strings-first-time ((self e/part:part))
   (setf *strings-state* :idle)
   )
 
 (defmethod strings-react ((self e/part:part) (e e/event:event))
-  (labels ((new-string () (make-token :kind :string :text (get-buffer) :position (get-position)))
+  (labels ((new-string () (make-token :kind :string :text (symbols-get-buffer) :position (symbols-get-position)))
            (check-eof ()
              (when (eq :eof (token-kind (e/event:data e)))
                (setf *strings-state* :done)))
@@ -37,8 +37,8 @@
                    (let ((c (token-text tok)))
                      (case c
                        (#\"
-                        (clear-buffer (token-position tok))
-                        (push-char-into-buffer c)
+                        (symbols-clear-buffer (token-position tok))
+                        (symbols-push-char-into-buffer c)
                         (pull)
                         (setf *strings-state* :collecting-string))
                        (otherwise (forward-token)
@@ -53,16 +53,16 @@
                 (let ((c (token-text tok)))
                   (case c
                     (#\"
-                     (push-char-into-buffer c)
+                     (symbols-push-char-into-buffer c)
                      (let ((str-token (new-string)))
-                       (clear-buffer (token-position tok))
+                       (symbols-clear-buffer (token-position tok))
                        (send! self :out str-token)
                        (setf *strings-state* :idle)))
                     (otherwise
-                     (push-char-into-buffer c)
+                     (symbols-push-char-into-buffer c)
                      (pull)))))
                ((eq :EOF (token-kind tok))
-                (send! self :error (format nil "incomplete string at ~A" (get-position))))
+                (send! self :error (format nil "incomplete string at ~A" (symbols-get-position))))
                (t (assert nil)))))
       
       (:done
--- svg/back-end/symbols.lisp
+++ svg/back-end/symbols.lisp
@@ -3,63 +3,74 @@
 ; (:code symbols (:token) (:request :out :error) #'symbols-react #'symbols-first-time)
 
 (defparameter *symbols-buffer* nil)
-(defparameter *symbols-position* 0)
+(defparameter *symbols-start-position* 0)
 (defparameter *symbols-state* :idle)
 
-(defun clear-buffer (pos)
-  (setf *symbols-buffer* nil)
-  (setf *symbols-position* pos))
-
-(defun push-char-into-buffer (c)
-  (push c *symbols-buffer*))
-
-(defun get-buffer () (coerce (reverse *symbols-buffer*) 'string))
-(defun get-position () *symbols-position*)
+(defun symbols-get-buffer () (coerce (reverse *symbols-buffer*) 'string))
+(defun get-position () *symbols-start-position*)
 
 (defmethod symbols-first-time ((self e/part:part))
   (setf *symbols-state* :idle)
   )
 
 (defmethod symbols-react ((self e/part:part) (e e/event:event))
-  (flet ((new-symbol ()
-           (let ((new-token (make-token :kind :symbol :text (get-buffer) :position (get-position))))
-             new-token))
-         (pull () (send! self :request t))
-         (forward-token ()
-           (send-event! self :out e)))
-  (ecase *symbols-state*
-    (:idle
-     (ecase (e/event::sym e)
+  (labels ((push-char-into-buffer () (push (token-text (e/event:data e)) *symbols-buffer*))
+           (pull () (send! self :request t))
+           (forward-token () (send-event! self :out e))
+           (start-char-p () 
+             (when (eq :character (token-kind (e/event:data e)))
+               (let ((c (token-text (e/event:data e))))
+                 (or (and (char>= c #\A) (char<= c #\Z))
+                     (and (char>= c #\a) (char<= c #\z))))))
+           (follow-char-p ()
+             (when (eq :character (token-kind (e/event:data e)))
+               (let ((c (token-text (e/event:data e))))
+                 (or (and (char>= c #\A) (char<= c #\Z))
+                     (and (char>= c #\a) (char<= c #\z))
+                     (and (char>= c #\0) (char<= c #\9))))))
+           (action () (e/event::sym e))
+           (next-state (x) (setf *symbols-state* x))
+           (eof-p () (eq :eof (token-kind (e/event:data e))))
+           (clear-buffer ()
+             (setf *symbols-buffer* nil)
+             (setf *symbols-start-position* (token-position (e/event:data e))))
+           (release-buffer ()
+             (send! self :out (make-token :kind :symbol :text (symbols-get-buffer) :position (get-position))))
+           (release-and-clear-buffer ()
+             (release-buffer)
+             (clear-buffer))
+         )
+    
+    (ecase *symbols-state*
+      (:idle
+       (ecase (action)
        (:token
-        (let ((tok (e/event:data e)))
-          (cond ((eq :character (token-kind tok))
-                 (let ((c (token-text tok)))
-                   (if (or (and (char>= c #\A) (char<= c #\Z))
-                           (and (char>= c #\a) (char<= c #\z)))
-                       (progn
-                         (clear-buffer (token-position tok))
-                         (push-char-into-buffer c)
-                         (pull)
-                         (setf *symbols-state* :collecting-symbol))
-                     (forward-token))))
-                (t (forward-token)))))))
-    (:collecting-symbol
-        (let ((tok (e/event:data e)))
-          (cond ((eq :character (token-kind tok))
-                 (let ((c (token-text tok)))
-                   (if (or (and (char>= c #\A) (char<= c #\Z))
-                           (and (char>= c #\a) (char<= c #\z))
-                           (and (char>= c #\0) (char<= c #\9)))
-                       (progn
-                         (push-char-into-buffer c)
-                         (pull))
-                     (progn
-                       (let ((str-token (new-symbol)))
-                         (clear-buffer (token-position tok))
-                         (send! self :out str-token)
-                         (forward-token)
-                         (setf *symbols-state* :idle))))))
-                (t (forward-token))))))))
-     
-
-     
+        (if (eof-p)
+            (progn
+              (forward-token)
+              (next-state :done))
+          (if (start-char-p)
+              (progn
+                (push-char-into-buffer)
+                (pull)
+                (next-state :collecting-symbol))
+            nil)))))
+      (:collecting-symbol
+       (ecase (action)
+         (:token
+          (if (eof-p)
+              (progn
+                (release-and-clear-buffer)
+                (forward-token)
+                (next-state :done))
+            (if (follow-char-p)
+                (progn
+                  (push-char-into-buffer)
+                  (pull))
+              (progn
+                (release-and-clear-buffer)
+                (forward-token)
+                (next-state :idle)))))))
+      (:done
+       (send! self :error (format nil "symbols finished, but received ~S" e))))))
+      
