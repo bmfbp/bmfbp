70d09f621fb77977000ab9c173f08cc7477c4ea3
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-20 08:51:06 -0500

    typos fixed (symbols -> strings)

--- svg/back-end/strings.lisp
+++ svg/back-end/strings.lisp
@@ -6,22 +6,22 @@
 (defparameter *strings-position* 0)
 (defparameter *strings-state* :idle)
 
-(defun symbols-clear-buffer (pos)
+(defun strings-clear-buffer (pos)
   (setf *strings-buffer* nil)
   (setf *strings-position* pos))
 
-(defun symbols-push-char-into-buffer (c)
+(defun strings-push-char-into-buffer (c)
   (push c *strings-buffer*))
 
-(defun symbols-get-buffer () (coerce (reverse *strings-buffer*) 'string))
-(defun symbols-get-position () *strings-position*)
+(defun strings-get-buffer () (coerce (reverse *strings-buffer*) 'string))
+(defun strings-get-position () *strings-position*)
 
 (defmethod strings-first-time ((self e/part:part))
   (setf *strings-state* :idle)
   )
 
 (defmethod strings-react ((self e/part:part) (e e/event:event))
-  (labels ((new-string () (make-token :kind :string :text (symbols-get-buffer) :position (symbols-get-position)))
+  (labels ((new-string () (make-token :kind :string :text (strings-get-buffer) :position (strings-get-position)))
            (check-eof ()
              (when (eq :eof (token-kind (e/event:data e)))
                (setf *strings-state* :done)))
@@ -37,8 +37,8 @@
                    (let ((c (token-text tok)))
                      (case c
                        (#\"
-                        (symbols-clear-buffer (token-position tok))
-                        (symbols-push-char-into-buffer c)
+                        (strings-clear-buffer (token-position tok))
+                        (strings-push-char-into-buffer c)
                         (pull)
                         (setf *strings-state* :collecting-string))
                        (otherwise (forward-token)
@@ -53,16 +53,16 @@
                 (let ((c (token-text tok)))
                   (case c
                     (#\"
-                     (symbols-push-char-into-buffer c)
+                     (strings-push-char-into-buffer c)
                      (let ((str-token (new-string)))
-                       (symbols-clear-buffer (token-position tok))
+                       (strings-clear-buffer (token-position tok))
                        (send! self :out str-token)
                        (setf *strings-state* :idle)))
                     (otherwise
-                     (symbols-push-char-into-buffer c)
+                     (strings-push-char-into-buffer c)
                      (pull)))))
                ((eq :EOF (token-kind tok))
-                (send! self :error (format nil "incomplete string at ~A" (symbols-get-position))))
+                (send! self :error (format nil "incomplete string at ~A" (strings-get-position))))
                (t (assert nil)))))
       
       (:done
