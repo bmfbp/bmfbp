9f75ce9f822a084e23be3087be6f32caeb0dfa02
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-21 11:12:24 -0500

    don't pull if token marked special

--- svg/back-end/dumper.lisp
+++ svg/back-end/dumper.lisp
@@ -7,7 +7,7 @@
   )
 
 (defmethod dumper-react ((self e/part:part) (e e/event:event))
-  (format *standard-output* "~&dumper ~S   ~S ~S~%" *dumper-state* (e/event::sym e) (e/event:data e))
+  ;(format *standard-output* "~&dumper ~S   ~S ~S~%" *dumper-state* (e/event::sym e) (e/event:data e))
   (flet ((pull (id) (send! self :request id)))
     (let ((tok (e/event::data e))
           (no-print '(:ws :newline :eof)))
@@ -25,11 +25,9 @@
                                      (if (member (token-kind tok) no-print) "." (token-text tok))))
             (if (eq :EOF (token-text tok))
                 (setf *dumper-state* :done)
-              (if (eq :character (token-text tok))
-                  (pull :dumper2)
-                (if (> (length (token-text tok)) 1)
-                    (pull :dumper3)))))))
-      
+              (unless (token-special tok)
+                (pull :dump2))))))
+
         (:done
          (send! self :error (format nil "dumper done, but got ~a pos:~a c:~a" (token-kind tok) (token-position tok)
                                     (if (member (token-kind tok) no-print) "." (token-text tok)))))))))
