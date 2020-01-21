19ad58f0e5e0af1c246c5b3d36a81fa549ad08de
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-21 10:05:23 -0500

    don't pull if length of token is 1 or less

--- svg/back-end/dumper.lisp
+++ svg/back-end/dumper.lisp
@@ -8,24 +8,28 @@
 
 (defmethod dumper-react ((self e/part:part) (e e/event:event))
   (format *standard-output* "~&dumper ~S   ~S ~S~%" *dumper-state* (e/event::sym e) (e/event:data e))
-  (let ((tok (e/event::data e))
-        (no-print '(:ws :newline :eof)))
-    (ecase *dumper-state*
-      (:idle
-       (ecase (e/event::sym e)
-         (:start
-          (send! self :request :dumper1)
-          (setf *dumper-state* :dumping))))
+  (flet ((pull (id) (send! self :request id)))
+    (let ((tok (e/event::data e))
+          (no-print '(:ws :newline :eof)))
+      (ecase *dumper-state*
+        (:idle
+         (ecase (e/event::sym e)
+           (:start
+            (pull :dumper1)
+            (setf *dumper-state* :dumping))))
 
-      (:dumping
-       (ecase (e/event::sym e)
-         (:in
-          (send! self :out (format nil "~a pos:~a c:~a" (token-kind tok) (token-position tok)
-                                   (if (member (token-kind tok) no-print) "." (token-text tok))))
-          (if (eq :EOF (token-text tok))
-              (setf *dumper-state* :done)
-            (send! self :request :dumper2)))))
+        (:dumping
+         (ecase (e/event::sym e)
+           (:in
+            (send! self :out (format nil "~a pos:~a c:~a" (token-kind tok) (token-position tok)
+                                     (if (member (token-kind tok) no-print) "." (token-text tok))))
+            (if (eq :EOF (token-text tok))
+                (setf *dumper-state* :done)
+              (if (eq :character (token-text tok))
+                  (pull :dumper2)
+                (if (> (length (token-text tok)) 1)
+                    (pull :dumper3)))))))
       
-      (:done
-       (send! self :error (format nil "dumper done, but got ~a pos:~a c:~a" (token-kind tok) (token-position tok)
-                                (if (member (token-kind tok) no-print) "." (token-text tok))))))))
+        (:done
+         (send! self :error (format nil "dumper done, but got ~a pos:~a c:~a" (token-kind tok) (token-position tok)
+                                    (if (member (token-kind tok) no-print) "." (token-text tok)))))))))
