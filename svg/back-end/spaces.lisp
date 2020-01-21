5b3d002a206219d64a8c7d2ce3df8d426fd6aa1f
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-21 14:02:27 -0500

    buffer spaces, create a single token for each ws run

--- svg/back-end/spaces.lisp
+++ svg/back-end/spaces.lisp
@@ -2,24 +2,74 @@
 
 ; (:code spaces (:token) (:request :out :error) #'spaces-react #'spaces-first-time)
 
+(defparameter *spaces-buffer* nil)
+(defparameter *spaces-start-position* 0)
 (defparameter *spaces-state* :idle)
+(defparameter *spaces-token-pulled-p* nil)
+
+(defun spaces-get-buffer () (coerce (reverse *spaces-buffer*) 'string))
+(defun spaces-get-position () *spaces-start-position*)
 
 (defmethod spaces-first-time ((self e/part:part))
   (setf *spaces-state* :idle)
   )
 
 (defmethod spaces-react ((self e/part:part) (e e/event:event))
-  (flet ((new-space (pos) (make-token :kind :ws :text " " :position pos))
-         (forward-token () (send-event! self :out e)))
+  (labels ((push-char-into-buffer () (push (token-text (e/event:data e)) *spaces-buffer*))
+           (pull () (send! self :request :spaces))
+           (forward-token (&key (pulled-p nil)) (send-event! self :out e))
+           (start-char-p () 
+             (when (eq :character (token-kind (e/event:data e)))
+               (let ((c (token-text (e/event:data e))))
+                 (member c '(#\Space #\Newline #\Tab #\Page)))))
+           (follow-char-p ()
+             (when (eq :character (token-kind (e/event:data e)))
+               (let ((c (token-text (e/event:data e))))
+                 (member c '(#\Space #\Newline #\Tab #\Page)))))
+           (action () (e/event::sym e))
+           (next-state (x) (setf *spaces-state* x))
+           (eof-p () (eq :eof (token-kind (e/event:data e))))
+           (clear-buffer ()
+             (setf *spaces-buffer* nil)
+             (setf *spaces-start-position* (token-position (e/event:data e))))
+           (release-buffer ()
+             (send! self :out (make-token :kind :ws :text " " :position (spaces-get-position) :pulled-p t)))
+           (release-and-clear-buffer ()
+             (release-buffer)
+             (clear-buffer))
+         )
+
+    ;(format *standard-output* "~&spaces in state ~S gets ~S ~S~%" *spaces-state* (token-kind (e/event:data e)) (token-text (e/event:data e)))
     (ecase *spaces-state*
       (:idle
-       (ecase (e/event::sym e)
+       (ecase (action)
          (:token
-          (let ((tok (e/event:data e)))
-            (cond ((eq :character (token-kind tok))
-                   (let ((c (token-text tok)))
-                     (case c
-                       ((#\Space #\Newline #\Tab #\Page)
-                        (send! self :out (new-space (token-position tok))))
-                       (otherwise (forward-token)))))
-                  (t (forward-token))))))))))
+          (if (eof-p)
+              (progn
+                (forward-token)
+                (next-state :done))
+            (if (start-char-p)
+                (progn
+                  (push-char-into-buffer)
+                  (pull)
+                  (next-state :collecting-ws))
+              (forward-token))))))
+      (:collecting-ws
+       (ecase (action)
+         (:token
+          (if (eof-p)
+              (progn
+                (release-and-clear-buffer)
+                (forward-token)
+                (next-state :done))
+            (if (follow-char-p)
+                (progn
+                  (push-char-into-buffer)
+                  (pull))
+              (progn
+                (release-and-clear-buffer)
+                (forward-token :pulled-p t)
+                (next-state :idle)))))))
+      (:done
+       (send! self :error (format nil "spaces finished, but received ~S" e))))))
+
