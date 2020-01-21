9b4c56c8d1ef8d45035ffe725a5b40e0e3938303
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-21 08:34:05 -0500

    tokenizer sends only one eof

--- svg/back-end/tokenize.lisp
+++ svg/back-end/tokenize.lisp
@@ -1,21 +1,35 @@
 (in-package :arrowgrams/compiler/back-end)
 
-(defparameter *stream* nil)
-(defparameter *position* 0)
+(defparameter *tokenizer-stream* nil)
+(defparameter *tokenizer-position* 0)
+(defparameter *tokenizer-state* :idle)
 
 (defmethod tokenize-first-time ((self e/part:part))
   )
 
 (defmethod tokenize-react ((self e/part:part) (e e/event:event))
-  (ecase (e/event::sym e)
-    (:start
-     (let ((str (alexandria:read-file-into-string (e/event:data e))))
-       (setf *stream* (make-string-input-stream str))
-       (setf *position* 0)))
+  (ecase *tokenizer-state*
+    (:idle
+     (ecase (e/event::sym e)
+       (:start
+        (let ((str (alexandria:read-file-into-string (e/event:data e))))
+          (setf *tokenizer-stream* (make-string-input-stream str))
+          (setf *tokenizer-position* 0)
+          (setf *tokenizer-state* :running)))))
 
-    (:pull
-     (format *standard-output* "~&tokenize in state idle gets :pull ~S~%" (e/event:data e))
-     (let ((c (read-char *stream* nil :EOF)))
-       (incf *position*)
-       (let ((tok (make-token :position *position* :kind (if (eq :EOF c) :EOF :character) :text c)))
-         (send! self :out tok))))))
+    (:running
+     (ecase (e/event::sym e)
+       (:pull
+        (format *standard-output* "~&tokenize in state idle gets :pull ~S~%" (e/event:data e))
+        (let ((c (read-char *tokenizer-stream* nil :EOF)))
+          (incf *tokenizer-position*)
+          (let ((reached-eof (eq :EOF c)))
+          (let ((tok (make-token :position *tokenizer-position* :kind (if readched-eof :EOF :character) :text c)))
+            (send! self :out tok)
+            (when reached-eof
+              (setf *tokenizer-state* :done))))))))
+
+    (:done
+     (send! self :error (format nil "tokenizer done, but received ~S" e)))))
+
+    
