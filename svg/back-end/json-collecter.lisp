2fdec80b8d928ff9cefd491c1dbb866b86f685c8
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-24 08:35:13 -0500

    push --> stack-push, pop --> stack-pop

--- svg/back-end/json-collecter.lisp
+++ svg/back-end/json-collecter.lisp
@@ -1,1 +1,192 @@
+(in-package :arrowgrams/compiler/back-end/json-collecter)
+
+(defclass stack ()
+  ((stack :initform nil :accessor stack)))
+
+(defclass part ()
+  ((name :accessor name :initarg :name)
+   (kind :accessor kind)
+   (inputs :accessor inputs)
+   (outputs :accessor outputs)
+   (react :accessor react)
+   (first-time :accessor first-time)))  
+
+(defclass schematic (part)
+  ((parts :accessor parts)
+   (wiring :accessor wiring)))
+
+(defclass parser (arrowgrams/compiler/back-end::parser) ()
+  ((schematic-stack :accessor schematic-stack)
+   (list-stack :accessor list-stack)
+   (part-stack :accessor part-stack)
+   (wire-stack :accessor wire-stack)
+   (top-schematic :accessor top-schematic)
+   (parts :initform (make-hash-table :test 'equal) :accessor parts)
+   (wires :initform (make-hash-table) :accessor wires)))
+
+
+(defparameter *rules*
+"
+= <ir> 
+  :lpar                   schematic-open-new
+    <kind>                schematic-set-kind-from-string
+    <inputs>              schematic-set-inputs-from-pin-list-pop
+    <outputs>             schematic-set-outputs-from-pin-list-pop
+    <react>               schematic-set-react-from-string
+    <first-time>          schematic-set-first-time-from-string
+    <part-declarations>   schematic-set-parts-from-part-stack-pop
+    <wiring>              schematic-set-wiring-from-wire-stack-pop
+                          schematic-close
+  :rpar
+
+
+= <inputs>
+                                        list-open-new
+  [ ?symbol :symbol symbol-must-be-nil  
+  | ?lpar :lpar <pin-list> :rpar]       list-close
+
+= <outputs>                             list-open-new
+  [ ?symbol :symbol symbol-must-be-nil  
+  | ?lpar :lpar <pin-list> :rpar]       list-close
+
+= <part-declarations> 
+  :lpar <part-decl-list> :rpar
+
+= <wiring> 
+  :lpar
+    <wire-list>
+  :rpar
+
+= <pin-list> 
+  <ident-list>
+
+= <ident-list> 
+  :string                               list-add-string
+  [ ?string <ident-list> ]
+
+= <part-decl-list> 
+  [ ?lpar <part-decl> [ ?lpar <part-decl-list> ] | ! ]
+
+= <part-decl>
+  :lpar <name> <kind> <inputs> <outputs> <react> <first-time> :rpar
+
+= <name>
+  :string
+
+= <kind>
+  :string
+
+= <react>
+  :string
+
+= <first-time>
+  :string
+
+= <wire-list>
+  <wire> [ ?lpar <wire-list> ] 
+
+= <wire>
+  :lpar
+    :integer print-text
+    :lpar <part-pin-list> :rpar
+    :lpar <part-pin-list> :rpar
+  :rpar
+
+= <part-pin-list> 
+  :lpar <part> <pin> :rpar 
+  [ ?lpar <part-pin-list>]
+
+= <part>
+  :string
+= <pin>
+  :string
+"
+)
+
+(eval
+ (read-from-string
+  (cl-ppcre:regex-replace-all "SL::" (cl:write-to-string (sl:parse *rules*)) "")))
+
+;; parser support
+(defmethod must-see ((p parser) token)   (arrowgrams/compiler/back-end:need p token))
+(defmethod look-ahead ((p parser) token)   (arrowgrams/compiler/back-end:look-ahead-p p token))
+(defmethod output ((p parser) str)   (arrowgrams/compiler/back-end:emit p str))
+(defmethod need-nil-symbol ((p parser) str)   (arrowgrams/compiler/back-end:emit p str))
+(defmethod call-external ((p parser) func)  (cl:apply func (list p)))
+(defmethod call-rule ((p parser) func)  (cl:apply func (list p)))
+
+;; mechanisms used in *rules* above
+(defmethod print-text ((p parser))
+  (format (arrowgrams/compiler/back-end:output-stream p)
+          "~a"
+          (arrowgrams/compiler/back-end:token-text (arrowgrams/compiler/back-end:accepted-token p))))
+(defmethod nl ((p parser))
+  (format (arrowgrams/compiler/back-end:output-stream p) "~%"))
+
+(defmethod symbol-must-be-nil ((p parser))
+  (arrowgrams/compiler/back-end:accepted-symbol-must-be-nil p))
+
+;; mechanisms
+
+(defmethod stack-push ((self stack) item)
+  (cl:push item (stack self)))
+
+(defmethod stack-pop ((self stack))
+  (cl:pop (stack self)))
+
+(defmethod top ((self stack))
+  (first (stack self)))
+
+
+(defmethod schematic-open-new ((self parser))
+  (stack-push (make-instance 'schematic :name "self") (schematic-stack self)))
+
+(defmethod schematic-set-kind-from-string ((self parser))
+  (let ((str (get-accepted-token-text self)))
+    (let ((top (top (schematic-stack self))))
+      (setf (kind top) str))))
+
+(defmethod schematic-set-react-from-string ((self parser))
+  (let ((str (get-accepted-token-text self)))
+    (let ((top (top (schematic-stack self))))
+      (setf (react top) str))))
+
+(defmethod schematic-set-first-time-from-string ((self parser))
+  (let ((str (get-accepted-token-text self)))
+    (let ((top (top (schematic-stack self))))
+      (setf (first-time top) str))))
+
+(defmethod schematic-close ((self parser))
+  (setf (top-schematic self)
+        (stack-pop (schematic-stack self))))
+
+(defmethod schematic-set-inputs-from-pin-list-pop ((self parser))
+  (let ((list (stack-pop (list-stack self))))
+    (let ((top-schem (top (schematic-stack self))))
+      (setf (inputs top-schem) list))))
+
+(defmethod schematic-set-outputs-from-pin-list-pop ((self parser))
+  (let ((list (stack-pop (list-stack self))))
+    (let ((top-schem (top (schematic-stack self))))
+      (setf (inputs top-schem) list))))
+
+
+(defmethod schematic-set-parts-from-part-stack-pop ((self parser))
+  (let ((part (stack-pop (part-stack self))))
+    (let ((top-schem (top (schematic-stack self))))
+      (setf (gethash (name part) (parts self))
+            part))))
+
+(defmethod schematic-set-wires-from-part-stack-pop ((self parser))
+  (let ((part (stack-pop (part-stack self))))
+    (let ((top-schem (top (schematic-stack self))))
+      (setf (gethash (name part) (parts self))
+            part))))
+
+(defmethod list-open-new ((self parser))
+  (stack-push nil (list-stack self)))
+
+(defmethod list-close ((self parser))
+  ;; noop - leave TOP on stack
+  )
 
