b0018efee0a48b4c785705d6b8885d25de7bebc4
Paul Tarvydas <guitarvydas@gmail.com>
2020-02-06 10:01:31 -0500

    disable debug

--- svg/back-end/collector.lisp
+++ svg/back-end/collector.lisp
@@ -30,7 +30,7 @@
          (ecase (e/event::sym e)
            (:parse
             (let ((p (make-instance 'parser :owner self :token-stream (e/event::data e) :name "collector" )))
-              (debug-accept t)
+              (debug-accept nil)
               (ir-collector p 0)
               (let ((schem (top-schematic p)))
                 (unparse-schematic p schem)
