b8ded649783ce778134e11fc1e5184a491f73eed
Paul Tarvydas <guitarvydas@gmail.com>
2020-02-10 16:18:55 -0500

    used @data

--- svg/cl-compiler/sequencer.lisp
+++ svg/cl-compiler/sequencer.lisp
@@ -25,7 +25,7 @@
          (@send
           self
           :error
-          (format nil "SEQUENCER in state :idle expected :finished-reading, but got action ~S data ~S" pin (e/event:data self e)))))
+          (format nil "SEQUENCER in state :idle expected :finished-reading, but got action ~S data ~S" pin (@data self e)))))
       
          (:waiting-for-pipeline
           (if (eq pin :finished-pipeline)
