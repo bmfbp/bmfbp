5dc04e73d6f4b4d52719ebfc62c18af975bc53b5
Paul Tarvydas <guitarvydas@gmail.com>
2020-02-14 15:28:57 -0500

    added declare ignore

--- svg/cl-compiler/classes.lisp
+++ svg/cl-compiler/classes.lisp
@@ -1,6 +1,7 @@
 (in-package :arrowgrams/compiler/classes)
 
 (defmacro with-speechbubble-bb (id-list bb &body body)
+  (declare (ignore id-list))
   `(let ((left (cdr (second ,bb)))
          (top  (cdr (third ,bb)))
          (right  (cdr (fourth ,bb)))
@@ -8,6 +9,7 @@
      ,@body))
 
 (defmacro with-text-bb (id-list bb &body body)
+  (declare (ignore id-list))
   `(let ((id (cdr (first ,bb)))
          (left (cdr (third ,bb)))
          (top  (cdr (fourth ,bb)))
