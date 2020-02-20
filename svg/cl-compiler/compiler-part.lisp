b0a2e255fec724e39d2aced4d5a8b0a7edd2fd2a
Paul Tarvydas <guitarvydas@gmail.com>
2020-02-20 06:29:21 -0500

    ripped out all ellipse specific code

--- svg/cl-compiler/compiler-part.lisp
+++ svg/cl-compiler/compiler-part.lisp
@@ -16,31 +16,4 @@
   (compiler-part-initially self))
 
 (defmethod e/part:react ((self compiler-part) e)
-  (let ((pin (@pin self e))
-        (data (@data self e)))
-    (ecase (@get self :state)
-      (:idle
-       (if (eq pin :fb)
-           (@set self :fb data)
-         (if (eq pin :go)
-             (progn
-               (send-rules self)
-               (@send self :request-fb t)
-               (@set self :state :waiting-for-new-fb))
-           (@send
-            self
-            :error
-            (format nil "~a in state :idle expected :fb or :go, but got action ~S data ~S" (compiler-part-name self) pin (e/event:data e))))))
-
-      (:waiting-for-new-fb
-       (if (eq pin :fb)
-           (progn
-             (format *standard-output* "~&compiler-part~%")
-             (@set self :fb data)
-             (compiler-part-run self e)
-             (@send self :done t)
-             (@set self :state :idle))
-         (@send
-          self
-          :error
-          (format nil "~a in state :waiting-for-new-fb expected :fb, but got action ~S data ~S" (compiler-part-name self) pin (e/event:data e))))))))  
+  (compiler-part-run self e))
