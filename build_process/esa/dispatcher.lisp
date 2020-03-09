adbc9ca0d946c2101925fa037477987c0b5a5726
Paul Tarvydas <guitarvydas@gmail.com>
2020-03-09 17:53:47 -0400

    rm files

--- build_process/esa/dispatcher.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :arrowgrams)
-
-(defclass dispatcher ()
-  ((all-parts :accessor all-parts :initform nil)
-   (top-part :accessor top-part :initform nil)))
--- build_process/esa/part.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :arrowgrams)
-
-(defclass runtime/node ()
-  ((kind :accessor kind :initarg :kind)
-   (input-queue :accessor input-queue :initform nil)
-   (output-queue :accessor output-queue :initform nil)
-   (parts :accessor parts :initform (make-hash-table :test 'equal))
-   (parent :accessor parent :initarg :parent)))
-
