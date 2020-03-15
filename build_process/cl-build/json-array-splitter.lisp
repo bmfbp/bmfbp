bbddffd48b8d3e000cb307175ff74f5167d680f5
Paul Tarvydas <guitarvydas@gmail.com>
2020-03-15 10:53:00 -0400

    change json-array-splitter to send metadata before graph

--- build_process/cl-build/json-array-splitter.lisp
+++ build_process/cl-build/json-array-splitter.lisp
@@ -1,19 +1,48 @@
 (in-package :arrowgrams/build)
 
 (defclass json-array-splitter (builder)
-  ())
+  ((state :accessor state)
+   (graph :accessor graph)))
 
 (defmethod e/part:first-time ((self json-array-splitter))
-)
+  (setf (state self) :idle)
+  (setf (graph self) nil))
 
 (defmethod e/part:react ((self json-array-splitter) e)
-  (ecase (@pin self e)
-    (:in
-     (let ((string-json-array (arrowgrams/compiler::strip-quotes (@data self e))))
-       (with-input-from-string (json-array string-json-array)
-         (let ((array (json:decode-json json-array)))
-           (@:loop
-             (@:exit-when (null array))
-             (let ((jstr (json:encode-json-alist-to-string (first array))))
-               (@send self :out jstr)
-               (pop array)))))))))
+  (flet ((split-array ()
+           (let ((string-json-array (arrowgrams/compiler::strip-quotes (@data self e))))
+             (with-input-from-string (json-array string-json-array)
+               (let ((array (json:decode-json json-array)))
+                 (@:loop
+                   (@:exit-when (null array))
+                   (let ((jstr (json:encode-json-alist-to-string (first array))))
+                     (@send self :out jstr)
+                     (pop array))))))))
+
+    ;; ensure that array is split and sent out before graph
+
+    (ecase (state self)
+      (:idle
+       (ecase (@pin self e)
+         (:array
+          (split-array)
+          (setf (state self) :wait-for-graph))
+         
+         (:json
+          (setf (graph self) (@data self e))
+          (setf (state self) :wait-for-array))))
+
+       (:wait-for-graph
+        (ecase (@pin self e)
+          (:json
+           (@send self :graph (@data self e))
+           (setf (state self) :idle))))
+
+       (:wait-for-array
+        (ecase (@pin self e)
+          (:array
+           (split-array)
+           (@send self :graph (graph self))
+           (setf (state self) :idle))))
+
+          )))
