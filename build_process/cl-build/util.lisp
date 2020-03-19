b00f2333ceed54fe063377abb3fa7e5b4fc3d90d
Paul Tarvydas <guitarvydas@gmail.com>
2020-03-19 08:36:27 -0400

    replace newlines with .

--- build_process/cl-build/util.lisp
+++ build_process/cl-build/util.lisp
@@ -3,5 +3,9 @@
 (defun chop-str (s)
   (if (and (stringp s)
            (> (length s) 6))
-      (concatenate 'string (subseq s 0 50) " ...")
+      (concatenate 'string (subseq (replace-newline s) 0 50) " ...")
     s))
+
+(defun replace-newline (s)
+  (cl-ppcre:regex-replace-all "
+" s "."))
