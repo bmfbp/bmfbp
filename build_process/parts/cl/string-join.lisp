35aa435de73a89c6aa92b91e9c49a7b4b74d70f7
Paul Tarvydas <guitarvydas@gmail.com>
2020-03-28 06:25:44 -0400

    elongated debug message

--- build_process/parts/cl/string-join.lisp
+++ build_process/parts/cl/string-join.lisp
@@ -9,7 +9,7 @@
   (setf (state self) :idle))
 
 (defmethod react ((self string-join) (e event))
-(format *standard-output* "~&react ~s~%" (name-in-container self))
+(format *standard-output* "~&react ~s with ~s ~s ~s~%" (name-in-container self) (part-name (partpin e)) (pin-name (partpin e)) (data e))
   (flet ((send-both ()
 	   (let ((out-e (make-instance 'event))
                  (pp (make-instance 'part-pin)))
