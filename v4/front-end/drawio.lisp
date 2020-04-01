5efa69e55f6d61585b571ba0c60c41b8a98b1958
Paul Tarvydas <guitarvydas@gmail.com>
2020-04-01 10:46:59 -0400

    v4 baseline

---
+++ v4/front-end/drawio.lisp
@@ -0,0 +1,17 @@
+(in-package :arrowgrams/compiler)
+
+(defclass front-end (compiler-part) ())
+
+(defmethod e/part:first-time ((self front-end))
+  (call-next-method))
+
+(defmethod e/part:react ((self front-end) e)
+(format *standard-output* "~&v4 compiler drawio ~s ~S~%" (@pin self e) (@data self e))
+  (ecase (state self)
+    (:idle
+     (let ((filename (cl-event-passing-user:@data self e)))
+       (let ((s (cl-user::front-end-main filename)))
+         (let ((strm (cl:make-string-input-stream s)))
+           #+nil(with-open-file (f "/Users/tarvydas/test.pro" :direction :output :if-exists :supersede)
+             (write s :stream f))
+           (cl-event-passing-user:@send self :output-string-stream strm)))))))
