4c0a516d2f3b4564da36d6425c8706ba6a6cb6b5
Paul Tarvydas <guitarvydas@gmail.com>
2020-04-04 09:01:51 -0400

    wip: wokring js expr

--- build_process/esa/test.lisp
+++ build_process/esa/test.lisp
@@ -1,35 +1,56 @@
+;; (ql:quickload :loops)
+
 (defparameter *expr*
-  '(:call (:field (:call (:field self y) a b) z) c d))
+  '(:call (:field (:call (:field "self" "y") "a" "b") "z") "c" "d"))
 
-(defun emit-as-js (expr s)
-  (unless (null expr)
-    (let ((kind (first expr))
-	  (inner (if (listp (second expr))
-		    (emit-as-js (second expr) s)
-		   (second expr)))
-	  (args (rest (cddr expr))))
-    (ecase kind
-      (:call
-       (format s "~s(" inner)
-       (@:loop
-	 (@:exit-when (null args))
-	 (let ((arg (pop args)))
-	   (format s "~s" arg)	   
-	   (when args
-	     (format s ","))))
-       (format s ")~%)"))
-      (:field
-       (format s "~s.~s" inner (third expr)))
-       ))))
+(defun emit-expr-as-js (expr s)
+  (flet ((print-args ()
+	   (let ((args (rest (cdr expr))))
+	     (format s "(")
+	     (@:loop
+	       (@:exit-when (null args))
+	       (let ((arg (pop args)))
+		 (format s "~a" arg)	   
+		 (when args
+		   (format s ","))))
+	     (format s ")"))))
+    (if (or (stringp (second expr))
+	    (symbolp (second expr)))
+	(ecase (first expr)
+	  (:field (format s "~a.~a" (second expr) (third expr)))
+	  (:call  (print-args)))
+	(progn
+	  (emit-expr-as-js (second expr) s)
+	  (ecase (first expr)
+	    (:field (format s ".~a" (third expr)))
+	    (:call  (print-args)))))))
 
-(defun emit-as-cl (expr s)
-  (format s "~&cl niy ~s~%" expr))
+(defun expr-as-cl (expr)
+(format *standard-output* "~&expr=~s~%" expr)
+  (flet ((u(x) (if (stringp x)
+		   (intern (string-upcase x))
+		   x)))
+    (if (or (stringp (second expr))
+	    (symbolp (second expr)))
+	(ecase (first expr)
+	  (:field `(slot-value ,(u (second expr)) ',(u (third expr))))
+	  (:call  `(:call1 ,@(rest (cddr expr)))))
+	(progn
+	  (let ((inner (u (expr-as-cl (second expr)))))
+	    (ecase (first expr)
+	      (:field `(slot-value ,inner ',(u (third expr))))
+	      (:call  `(:call2 ,inner ,@(mapcar #'u (rest (rest expr)))))))))))
+
+(defun test-both (e)
+  (with-output-to-string (s)
+    (format s "~%")
+    (emit-expr-as-js e s)
+    (format s "~%~%")
+    (let ((cl-expr (expr-as-cl e)))
+      (format s "~&~s~%" cl-expr)
+      cl-expr)))
 
 (defun test ()
-  (with-output-to-string (s)
-    (emit-as-js *expr* s)
-    (format s "~%~%")
-    (emit-as-cl *expr* s)))
-
+  (test-both *expr*))
 
     
