feaed2fd2fe42a19b5f65e5d8d766cd15a2308e2
Paul Tarvydas <guitarvydas@gmail.com>
2020-05-28 12:57:29 -0400

    wip: use path() for transpiler

--- build_process/esa-transpiler/make.lisp
+++ build_process/esa-transpiler/make.lisp
@@ -39,7 +39,8 @@
   (stack-dsl:initialize-types (arrowgrams/esa-transpiler:path \"exprtypes.json\"))
   #+nil(arrowgrams/esa-transpiler::trace-mechs)
   #+nil(arrowgrams/esa-transpiler::trace-rules)
-  (arrowgrams/esa-transpiler::transpile-esa-to-string \"test.esa\" :tracing-accept t) 
+  (arrowgrams/esa-transpiler:path \"test.esa\" :tracing-accept t) 
+  #+nil(arrowgrams/esa-transpiler::transpile-esa-to-string \"test.esa\" :tracing-accept t) 
 ")
 
 (defun make ()
