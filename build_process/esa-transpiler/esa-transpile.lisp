f4a20c5a7e6187de3e8805e40f46abf1b4ea143a
Paul Tarvydas <guitarvydas@gmail.com>
2020-06-05 08:57:11 -0400

    stitch outputs from pass1 and pass2 together

--- build_process/esa-transpiler/esa-transpile.lisp
+++ build_process/esa-transpiler/esa-transpile.lisp
@@ -10,31 +10,19 @@
 	(pasm:initially p token-stream)
 	(let ((pasm::*pasm-accept-tracing* tracing-accept))
 	  (esa-dsl-pass1 p))  ;; call top rule of 1st pass to generate dsl1.lisp
-	(pasm:initially p token-stream)
-	(let ((pasm::*pasm-accept-tracing* tracing-accept))
-	  (esa-dsl-pass2 p))  ;; call top rule of 2nd pass to generate dsl2.lisp
-	(let ((result (get-output-stream-string (pasm:output-string-stream p))))
-	  (concatenate 'string 
-		       (format nil "(in-package :esa)~%~%")
-		       result)
-	  result)))))
+	(let ((result-pass1 (get-output-stream-string (pasm:output-string-stream p))))
+	  (pasm:initially p token-stream)
+	  (let ((pasm::*pasm-accept-tracing* tracing-accept))
+	    (esa-dsl-pass2 p))  ;; call top rule of 2nd pass to generate dsl2.lisp
+	  (let ((result-pass2 (get-output-stream-string (pasm:output-string-stream p))))
+	    (let ((final (concatenate 'string 
+				      (format nil "(in-package :esa)~%~%")
+				      result-pass1
+				      result-pass2)))
+	      final)))))))
 
 (defun transpile-esa-to-file (esa-input-filename output-filename &key (tracing-accept nil))
   (let ((str (transpile-esa-to-string esa-input-filename :tracing-accept tracing-accept)))
     (with-open-file (outf output-filename :direction :output :if-exists :supersede :if-does-not-exist :create)
       (write-string str outf))))
-
-(defun test-transpile-esa-to-string (esa-input-filename &key (tracing-accept nil))
-  (let ((in-string (alexandria:read-file-into-string esa-input-filename)))
-    (let ((token-stream (scanner:scanner in-string)))
-      (let ((p (make-instance 'arrowgrams/esa-transpiler::parser)))
-	(pasm:initially p token-stream)
-	(let ((pasm::*pasm-accept-tracing* tracing-accept))
-	  (tester p)  ;; call test rule
-	  )
-	(let ((result (get-output-stream-string (pasm:output-string-stream p))))
-	  (concatenate 'string 
-		       (format nil "(in-package :esa)~%~%")
-		       result)
-	  result)))))
 
