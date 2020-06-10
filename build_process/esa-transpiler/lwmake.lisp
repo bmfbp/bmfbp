72cb110df70c9d18fbe530d7102a02a8f69744f1
Paul Tarvydas <guitarvydas@gmail.com>
2020-06-10 17:54:54 -0400

    wip: mods to lwmake.lisp

--- build_process/esa-transpiler/lwmake.lisp
+++ build_process/esa-transpiler/lwmake.lisp
@@ -1,20 +1,11 @@
 (in-package :cl-user)
 
+(uiop:run-program "~/quicklisp/local-projects/rm.bash")
 (proclaim '(optimize (debug 3) (safety 3) (speed 0)))
 
-#|
-cd to ~/quicklisp/local-projects/hier, then run awk -f 12.awk <12.txt >12.lisp , then copy 12.lisp into mech-tester.lisp and manually edit the result to insert names
-|#
-
-
-(uiop:run-program "rm -rf ~/.cache/common-lisp/")
-(proclaim '(optimize (debug 3) (safety 3) (speed 0)))
-
-(ql:quickload :stack-dsl)
-(ql:quickload :stack-dsl/use)
-(ql:quickload :parsing-assembler/use)
-
-(defun gen ()
+(defun l1 ()
+  (ql:quickload :stack-dsl)
+  (ql:quickload :stack-dsl/use)
   (stack-dsl:transpile-stack 
    (asdf:system-relative-pathname :arrowgrams "build_process/esa-transpiler/exprtypes.dsl")
    "CL-USER"
@@ -23,7 +14,11 @@ cd to ~/quicklisp/local-projects/hier, then run awk -f 12.awk <12.txt >12.lisp ,
    "ARROWGRAMS/ESA-TRANSPILER"
    "CL-USER"
    (asdf:system-relative-pathname :arrowgrams "build_process/esa-transpiler/mechanisms.lisp")
-   )
+   ))
+
+
+(defun l2 ()
+  (ql:quickload :parsing-assembler/use)
   (pasm:pasm-to-file 
    "ARROWGRAMS/ESA-TRANSPILER"
    (asdf:system-relative-pathname :arrowgrams "build_process/esa-transpiler/dsl0.pasm")
@@ -40,9 +35,10 @@ cd to ~/quicklisp/local-projects/hier, then run awk -f 12.awk <12.txt >12.lisp ,
    (asdf:system-relative-pathname :arrowgrams "build_process/esa-transpiler/dsl2.lisp")
    "-PASS2"))
 
-(ql:quickload :arrowgrams/esa-transpiler)
-
-(defun lrun ()
+(defun l3 ()
+  (ql:quickload :arrowgrams/esa-transpiler)
+  
+  (proclaim '(optimize (debug 3) (safety 3) (speed 0)))
   (load (arrowgrams/esa-transpiler::path "package.lisp"))
   (load (arrowgrams/esa-transpiler::path "classes.lisp"))
   (load (arrowgrams/esa-transpiler::path "dsl0.lisp"))
@@ -56,23 +52,13 @@ cd to ~/quicklisp/local-projects/hier, then run awk -f 12.awk <12.txt >12.lisp ,
   (load (arrowgrams/esa-transpiler::path "esa-transpile.lisp"))
   (load (arrowgrams/esa-transpiler::path "trace-rules.lisp"))
   (load (arrowgrams/esa-transpiler::path "trace-mechs.lisp"))
-  (stack-dsl:initialize-types (arrowgrams/esa-transpiler:path "exprtypes.json"))
-  (arrowgrams/esa-transpiler::transpile-esa-to-string 
-   (arrowgrams/esa-transpiler:path "esa-test.dsl")
-   :tracing-accept t))
+)
+  
+(defun l4 ()
+  (ql:quickload :arrowgrams/esa-transpiler/tester))
 
 (defun run ()
   (stack-dsl:initialize-types (arrowgrams/esa-transpiler:path "exprtypes.json"))
   (arrowgrams/esa-transpiler::transpile-esa-to-string 
    (arrowgrams/esa-transpiler:path "esa-test.dsl")
-   :tracing-accept t))
-
-#|
-after loading, do:
-edit mech-tester.lisp to include latest 12.txt (from hier)
-edit mech-tester.lisp to insert names, eg.
-(setf (scanner:token-text (pasm:accepted-token p)) "self")
-run test
-(test-esa-to-file "test.esa" "test.lisp" t) 
-(esa-input-filename output-filename &key (tracing-accept nil))
-|#
+   :tracing-accept nil))
