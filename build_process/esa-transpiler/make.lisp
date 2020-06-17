38dfc15bd79ea82476d314723995bedc0c0331a9
Paul Tarvydas <guitarvydas@gmail.com>
2020-06-17 14:55:14 -0400

    wip: alpha using esa.dsl (in esa-test1.dsl)

--- build_process/esa-transpiler/make.lisp
+++ build_process/esa-transpiler/make.lisp
@@ -55,10 +55,11 @@
     (ql:quickload :arrowgrams/esa-transpiler/tester)
   (let ()
     (stack-dsl:initialize-types (arrowgrams/esa-transpiler:path \"exprtypes.json\"))
-    (arrowgrams/esa-transpiler::transpile-esa-to-string 
-      (arrowgrams/esa-transpiler:path \"esa-test1.dsl\")
-      #| (arrowgrams/esa-transpiler:path \"../esa/esa.dsl\")    |#
-      :tracing-accept nil))
+    (let ((result 
+           (arrowgrams/esa-transpiler::transpile-esa-to-string 
+             (arrowgrams/esa-transpiler:path \"esa-test1.dsl\")
+            :tracing-accept nil)))
+      (format *standard-output* \"~&result: ~a~%\" result)))
 ")
 
 
