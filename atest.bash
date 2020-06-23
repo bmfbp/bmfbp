4238ea3a7e82990ee04369f22ac7b3bd4e38fd12
Paul Tarvydas <guitarvydas@gmail.com>
2020-06-23 09:13:26 -0400

    cosmetic change to atest.bash

--- atest.bash
+++ atest.bash
@@ -1,13 +1,13 @@
 #~/bin/bash
 sbcl --noinform \
-      --eval '(uiop:run-program "~/quicklisp/local-projects/rm.bash")' \
-      --eval '(ql:register-local-projects)' \
-      --quit && \
-sbcl --noinform \
      --eval '(uiop:run-program "~/quicklisp/local-projects/rm.bash")' \
-     --eval '(ql:quickload :arrowgrams/build :silent t)' \
-     --eval '(arrowgrams/build::arrowgrams-to-json "helloworld")' \
-     --eval '(ql:quickload :arrowgrams/runner :silent t)' \
-     --eval '(arrowgrams/build::load-and-run-from-file (arrowgrams/build::json-graph-path "helloworld"))' \
-     --quit
+     --eval '(ql:register-local-projects)' \
+     --quit && \
+  sbcl --noinform \
+       --eval '(uiop:run-program "~/quicklisp/local-projects/rm.bash")' \
+       --eval '(ql:quickload :arrowgrams/build :silent t)' \
+       --eval '(arrowgrams/build::arrowgrams-to-json "helloworld")' \
+       --eval '(ql:quickload :arrowgrams/runner :silent t)' \
+       --eval '(arrowgrams/build::load-and-run-from-file (arrowgrams/build::json-graph-path "helloworld"))' \
+       --quit
 
