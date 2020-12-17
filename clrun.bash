ad7e4d12aff96f9d0cbd618b2de6327fd988f585
Paul Tarvydas <guitarvydas@gmail.com>
2020-12-17 09:22:40 -0500

    $1 shell parameter

--- clrun.bash
+++ clrun.bash
@@ -1,15 +1,9 @@
 #~/bin/bash
-echo "register" && \
-sbcl --noinform \
-     --eval '(uiop:run-program "~/quicklisp/local-projects/rm.bash")' \
-     --eval '(ql:register-local-projects)' \
-     --quit && \
- echo "" && \
- echo "run" && \
- echo "" && \
-   sbcl  --noinform \
-        --eval '(ql:quickload :arrowgrams/runner :silent nil)' \
-	--eval '(format *standard-output* "running sbcl~%")' \
-        --eval '(arrowgrams/build::load-and-run-from-file (arrowgrams/build::json-graph-path "helloworld"))' \
-        --eval '(cl-user::dispatcher-inject *dispatcher* "start" t)' \
-        --quit
+# usage: ./clrun.bash helloworld
+sbcl  --noinform \
+      --eval '(uiop:run-program "~/quicklisp/local-projects/rm.bash")' \
+      --eval '(ql:register-local-projects)' \
+      --eval '(ql:quickload :arrowgrams/runner :silent nil)' \
+      --eval "(arrowgrams/build::load-and-run-from-file (arrowgrams/build::json-graph-path \"$1\"))" \
+      --eval '(cl-user::dispatcher-inject *dispatcher* "start" t)' \
+      --quit
--- svg2json.bash
+++ svg2json.bash
@@ -1,14 +1,8 @@
 #~/bin/bash
 # usage: ./svg2json helloworld
-echo "register" && \
-sbcl --noinform \
-     --eval '(uiop:run-program "~/quicklisp/local-projects/rm.bash")' \
-     --eval '(ql:register-local-projects)' \
-     --quit && \
-echo "" && \
-echo "compile" && \
-echo "" && \
-  sbcl  --noinform \
-       --eval '(ql:quickload :arrowgrams/build :silent nil)' \
-       --eval '(arrowgrams/build::arrowgrams-to-json "$1")' \
-       --quit
+sbcl  --noinform \
+      --eval '(uiop:run-program "~/quicklisp/local-projects/arrowgrams/rm.bash")' \
+      --eval '(ql:register-local-projects)' \
+      --eval '(ql:quickload :arrowgrams/build :silent nil)' \
+      --eval "(arrowgrams/build::arrowgrams-to-json \"$1\")" \
+      --quit
