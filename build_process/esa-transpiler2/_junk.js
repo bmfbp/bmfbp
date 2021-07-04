82230d20c6b06d0d6b939c42e38090816c8d96f3
Paul Tarvydas <guitarvydas@gmail.com>
2021-07-04 18:11:57 -0400

    wip: makestacks still missing

---
+++ build_process/esa-transpiler2/_junk.js
@@ -0,0 +1,6 @@
+var s = new Set ();
+s.add ('a');
+s.add ('b');
+s.add ('b');
+s.add ('c');
+console.log(s);
--- build_process/esa-transpiler2/support.js
+++ build_process/esa-transpiler2/support.js
@@ -1,14 +1,41 @@
-function memoType(name) {
+function memotype(name) {
     let namelist = scopeGet('typememos');
-    namelist.push(name);
+    namelist.add(name);
     scopeModify('typememos',namelist);
     return "";
 }
 
 function existenceChecks() {
     let namelist = scopeGet('typememos');
-    return namelist.map(n => {
-	return `(stack-dsl::%ensure-existence '${n})\n`;
-    }).join('');
+    let a = [];
+    namelist.forEach(n => a.push (`(stack-dsl::%ensure-existence '${n})\n`));
+    return a.join('');
 }
 
+function boilerplate () {
+    return `(defclass %map-stack (stack-dsl::%typed-stack) ())
+(defclass %bag-stack (stack-dsl::%typed-stack) ())
+(defmethod initialize-instance :after ((self %map-stack) &key &allow-other-keys)
+  (setf (stack-dsl::%element-type self) "%map"))(defmethod initialize-instance :after ((self %bag-stack) &key &allow-other-keys)
+  (setf (stack-dsl::%element-type self) "%bag"))`;
+}
+
+function makestacks() {
+    return "*** makestacks ***";
+}
+
+// function old-makestacks() {
+//     let namelist = scopeGet('typememos');
+//     let a = [];
+//     namelist.forEach(n => a.push (
+// `
+// (input-${a} :accessor input-${a} :initform (make-instance '${a})
+// (output-${a} :accessor output-${a} :initform (make-instance '${a})
+// `
+//     ));
+//     return `
+// (defclass environment ()
+// ((%water-mark :accessor %water-mark :initform nil)
+// ${a.join('')}))`;
+// }
+    
